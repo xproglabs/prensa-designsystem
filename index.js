11a547d27827ac003bec64466417376f1136f990
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-04-07 22:55:50 -0300

    Build

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ Object.defineProperty(exports, '__esModule', { value: true });
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
 var React = _interopDefault(require('react'));
+var PropTypes = _interopDefault(require('prop-types'));
 
-var version = "1.1.0";
+var version = "1.1.1";
 
 var Forms = function Forms() {
   console.log('form ' + version);
@@ -18,5 +19,23 @@ var Version = function Version() {
   return /*#__PURE__*/React.createElement("p", null, "version: ", version);
 };
 
+var Icon = function Icon(data) {
+  return /*#__PURE__*/React.createElement("svg", data, data.children);
+};
+
+Icon.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  viewBox: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.array, PropTypes.object])
+};
+Icon.defaultProps = {
+  width: 24,
+  height: 24,
+  viewBox: '0 0 24 24'
+};
+
 exports.Forms = Forms;
+exports.Icon = Icon;
 exports.Version = Version;
