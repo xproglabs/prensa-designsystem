3bd10fa4aa9c69e6f05bbbf12a124f748da15f08
Fernando <fernando.bombassaro@gmail.com>
2020-05-16 18:26:54 -0300

    Adiciona classes de alinhamentos gerais

--- example/pages/index.js
+++ example/pages/index.js
@@ -7,6 +7,11 @@ const Example = () => {
     <React.Fragment>
       <Form />
       <Dialog />
+      <div className="w-100 mw-xl fx-cnt-btw">
+        <div className="">1</div>
+        <div className="">2</div>
+        <div className="">3</div>
+      </div>
     </React.Fragment>
   )
 }
