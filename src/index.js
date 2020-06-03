b45aceeea53e37ef0e83f019324c5c01acd30f5f
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-06-03 18:59:58 -0300

    Pipeline fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import Button from './Button';
 import Form from './Form';
-import Frame from './Frame';
 import Icon from './Icon';
 import Panel from './Panel';
 
 export {
   Button,
   Form,
-  Frame,
   Icon,
   Panel,
 };
