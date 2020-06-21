d5313e429c69f4947cb451983a68c301446bd5c9
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-06-21 19:17:05 -0300

    Ajusta stories dos Teasers

--- stories/Teaser.stories.js
+++ stories/Teaser.stories.js
@@ -1,3 +1,4 @@
+import {text, boolean, withKnobs} from '@storybook/addon-knobs';
 import React from 'react';
 
 import {Teaser, TeaserFeatured} from '../src/Teasers';
@@ -6,22 +7,43 @@ import {sassWrapper} from '../utils/SassWrapper';
 export default {
   title: 'Components | Teaser',
   component: Teaser,
-  decorators: [sassWrapper]
+  decorators: [withKnobs, sassWrapper]
 };
 
 export const Default = () => {
+  const hasImage = boolean('image', false);
+  const image = {
+    imageUrl: null,
+    captionAndByline: null
+  };
+  const content = {
+    title: text('title', 'The quick brown fox jumps over the lazy dog'),
+    subtitle: text('subtitle', 'Lorem ipsum sit dolor amet lorem situation'),
+    subject: text('subject', 'Assunto'),
+    image: hasImage && image
+  };
+  const status = {
+    loading: boolean('loading', false),
+    error: boolean('loading', false)
+  };
   return (
-    <Teaser title='The quick brown fox jumps over the lazy dog' subtitle='Lorem ipsum sit dolor amet lorem situation' subject='Subject' />
+    <Teaser content={content} status={status} />
   );
 };
 export const Featured = () => {
+  const hasImage = boolean('image', false);
+  const image = {
+    imageUrl: null,
+    captionAndByline: null
+  };
   const content = {
-    title: 'The quick brown fox jumps over the lazy dog',
-    subject: 'Subject'
+    title: text('title', 'The quick brown fox jumps over the lazy dog'),
+    subject: text('subject', 'Assunto'),
+    image: hasImage && image
   };
   const status = {
-    loading: false,
-    error: false
+    loading: boolean('loading', false),
+    error: boolean('loading', false)
   };
   return (
     <TeaserFeatured content={content} status={status} />
