fb863a961a6d5fd33a67d7a189793c50c9e5ec88
Fernando <fernando.bombassaro@gmail.com>
2020-06-28 14:52:09 -0300

    Build and version 1.2.1

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ Button.propTypes = {
   onClick: PropTypes.func
 };
 Button.defaultProps = {
-  children: 'Label',
   style: 'primary'
 };
 
@@ -98,221 +97,6 @@ var index = {
   Input: Input
 };
 
-var goToPage = function goToPage(location) {
-  return window.location = location;
-};
-
-var Block = function Block(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-block ".concat(props["class"])
-  }, props.children);
-};
-
-var ButtonPrimary = function ButtonPrimary(_ref) {
-  var children = _ref.children,
-      goTo = _ref.goTo;
-
-  var handeClick = function handeClick() {
-    return goTo ? goToPage(goTo) : false;
-  };
-
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-button-primary",
-    onClick: handeClick
-  }, children);
-};
-
-var ButtonSecondary = function ButtonSecondary(_ref2) {
-  var children = _ref2.children,
-      goTo = _ref2.goTo;
-
-  var handeClick = function handeClick() {
-    return goTo ? goToPage(goTo) : false;
-  };
-
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-button-secondary",
-    onClick: handeClick
-  }, children);
-};
-
-var Card = function Card(props) {
-  var handeClick = function handeClick() {
-    return goToPage('/frames/page');
-  };
-
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-card",
-    onClick: handeClick
-  }, /*#__PURE__*/React.createElement("div", {
-    className: "fr-card-image"
-  }, props.image), /*#__PURE__*/React.createElement("div", {
-    className: "fr-card-text"
-  }, props.children ? props.children : /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Line, {
-    "class": "card-title"
-  }), /*#__PURE__*/React.createElement(Line, {
-    "class": "card-title"
-  }), /*#__PURE__*/React.createElement(Line, {
-    "class": "card-lead"
-  }), /*#__PURE__*/React.createElement(FlexBtwn, null, /*#__PURE__*/React.createElement(Line, {
-    "class": "card-author"
-  }), /*#__PURE__*/React.createElement(Line, {
-    "class": "card-date"
-  })))));
-};
-
-var Container = function Container(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-container"
-  }, props.children);
-};
-
-var FX = function FX(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-fx"
-  }, props.children);
-};
-
-var FY = function FY(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-fy"
-  }, props.children);
-};
-
-var FlexBtwn = function FlexBtwn(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-flex-btwn"
-  }, props.children);
-};
-
-var FlexEnd = function FlexEnd(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-flex-end"
-  }, props.children);
-};
-
-var FlexStart = function FlexStart(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-flex-start"
-  }, props.children);
-};
-
-var Footer = function Footer(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-footer"
-  }, props.children);
-};
-
-var FooterLead = function FooterLead(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-footer-lead"
-  }, /*#__PURE__*/React.createElement("p", null), /*#__PURE__*/React.createElement("p", null), /*#__PURE__*/React.createElement("p", null), /*#__PURE__*/React.createElement("p", null), /*#__PURE__*/React.createElement("p", null));
-};
-
-var Grid = function Grid(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-grid"
-  }, props.children);
-};
-
-var ListView = function ListView(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-listview"
-  }, props.children);
-};
-
-var Line = function Line(props) {
-  return /*#__PURE__*/React.createElement("p", {
-    className: "fr-line ".concat(props["class"])
-  }, props.children);
-};
-
-var Logo = function Logo(_ref3) {
-  var children = _ref3.children,
-      goTo = _ref3.goTo;
-
-  var handeClick = function handeClick() {
-    return goTo ? goToPage(goTo) : false;
-  };
-
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-logo",
-    onClick: handeClick
-  }, children);
-};
-
-var Slide = function Slide(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-slide ".concat(props["class"])
-  }, props.children);
-};
-
-var SlideLead = function SlideLead(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-slide-lead"
-  }, props.children);
-};
-
-var SlideText = function SlideText(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-slide-text"
-  }, props.children ? props.children : /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Line, {
-    "class": "home-lead"
-  }), /*#__PURE__*/React.createElement(Line, {
-    "class": "home-lead"
-  }), /*#__PURE__*/React.createElement(Line, {
-    "class": "home-lead"
-  })));
-};
-
-var SlideTitle = function SlideTitle(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-slide-title"
-  }, props.value ? /*#__PURE__*/React.createElement(Text, {
-    "class": "home-title",
-    value: props.value
-  }) : /*#__PURE__*/React.createElement(Line, {
-    "class": "home-title mkp"
-  }));
-};
-
-var Text = function Text(props) {
-  return /*#__PURE__*/React.createElement("p", {
-    className: "fr-text ".concat(props.value ? props["class"] : "mkp")
-  }, props.value);
-};
-
-var Topbar = function Topbar(props) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "fr-topbar"
-  }, props.children);
-};
-
-var index$1 = {
-  Block: Block,
-  ButtonPrimary: ButtonPrimary,
-  ButtonSecondary: ButtonSecondary,
-  Card: Card,
-  Container: Container,
-  FX: FX,
-  FY: FY,
-  FlexBtwn: FlexBtwn,
-  FlexEnd: FlexEnd,
-  FlexStart: FlexStart,
-  Footer: Footer,
-  FooterLead: FooterLead,
-  Grid: Grid,
-  Line: Line,
-  ListView: ListView,
-  Logo: Logo,
-  Slide: Slide,
-  SlideText: SlideText,
-  SlideTitle: SlideTitle,
-  SlideLead: SlideLead,
-  Text: Text,
-  Topbar: Topbar
-};
-
 var Icon = function Icon(data) {
   return /*#__PURE__*/React.createElement("svg", data, data.children);
 };
@@ -330,6 +114,246 @@ Icon.defaultProps = {
   viewBox: '0 0 24 24'
 };
 
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
+var Block = function Block(props) {
+  var _classnames;
+
+  var alignBetween = props.alignBetween,
+      alignBottom = props.alignBottom,
+      alignCenter = props.alignCenter,
+      alignEvenly = props.alignEvenly,
+      alignLeft = props.alignLeft,
+      alignMiddle = props.alignMiddle,
+      alignRight = props.alignRight,
+      alignTop = props.alignTop,
+      bgColor = props.bgColor,
+      custom = props.custom,
+      functions = props.functions,
+      fColor = props.fColor,
+      fitH = props.fitH,
+      fitW = props.fitW,
+      m = props.m,
+      mb = props.mb,
+      ml = props.ml,
+      mr = props.mr,
+      mt = props.mt,
+      p = props.p,
+      pb = props.pb,
+      pl = props.pl,
+      pr = props.pr,
+      pt = props.pt,
+      row = props.row,
+      height = props.height,
+      width = props.width,
+      style = props.style;
+
+  var getStyle = function getStyle() {
+    switch (style) {
+      case '3-col':
+        return 'block-three-col';
+
+      case '4-col':
+        return 'block-four-col';
+
+      default:
+        return '';
+    }
+  };
+
+  var classes = classnames((_classnames = {
+    'ds-block': true,
+    'fx-align-between': !row && alignBetween,
+    'fx-align-bottom': !row && alignBottom,
+    'fx-align-center': !row && alignCenter,
+    'fx-align-evenly': !row && alignEvenly,
+    'fx-align-left': !row && alignLeft,
+    'fx-align-middle': !row && alignMiddle,
+    'fx-align-right': !row && alignRight,
+    'fx-align-top': !row && alignTop,
+    'fy-align-between': row && alignBetween,
+    'fy-align-bottom': row && alignBottom,
+    'fy-align-center': row && alignCenter,
+    'fy-align-evenly': row && alignEvenly,
+    'fy-align-left': row && alignLeft,
+    'fy-align-middle': row && alignMiddle,
+    'fy-align-right': row && alignRight,
+    'fy-align-top': row && alignTop,
+    'fx-dir-col': !row,
+    'fx-dir-row': row,
+    'fx-fit-h': fitH,
+    'fx-fit-w': fitW
+  }, _defineProperty(_classnames, "xp-bg-".concat(bgColor), bgColor), _defineProperty(_classnames, "xp-f-".concat(fColor), fColor), _defineProperty(_classnames, "xp-h-".concat(height), height), _defineProperty(_classnames, "xp-m-".concat(m), m), _defineProperty(_classnames, "xp-mb-".concat(mb), mb), _defineProperty(_classnames, "xp-ml-".concat(ml), ml), _defineProperty(_classnames, "xp-mr-".concat(mr), mr), _defineProperty(_classnames, "xp-mt-".concat(mt), mt), _defineProperty(_classnames, "xp-p-".concat(p), p), _defineProperty(_classnames, "xp-pb-".concat(pb), pb), _defineProperty(_classnames, "xp-pl-".concat(pl), pl), _defineProperty(_classnames, "xp-pr-".concat(pr), pr), _defineProperty(_classnames, "xp-pt-".concat(pt), pt), _defineProperty(_classnames, "xp-w-".concat(width), width), _defineProperty(_classnames, "".concat(custom), custom), _classnames));
+  var handleClick = functions.handleClick;
+  return /*#__PURE__*/React.createElement("div", {
+    className: classnames(classes, getStyle()),
+    onClick: handleClick && handleClick
+  }, props.children && props.children);
+};
+
+Block.propTypes = {
+  alignBetween: PropTypes.bool,
+  alignBottom: PropTypes.bool,
+  alignCenter: PropTypes.bool,
+  alignEvenly: PropTypes.bool,
+  alignLeft: PropTypes.bool,
+  alignMiddle: PropTypes.bool,
+  alignRight: PropTypes.bool,
+  alignTop: PropTypes.bool,
+  bgColor: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.array, PropTypes.object, PropTypes.node]),
+  custom: PropTypes.string,
+  functions: PropTypes.object,
+  fColor: PropTypes.string,
+  fitH: PropTypes.bool,
+  fitW: PropTypes.bool,
+  m: PropTypes.oneOf(['0', 'xs', 'sm', 'md', 'lg', 'xl']),
+  mb: PropTypes.oneOf(['0', 'xs', 'sm', 'md', 'lg', 'xl']),
+  ml: PropTypes.oneOf(['0', 'xs', 'sm', 'md', 'lg', 'xl']),
+  mr: PropTypes.oneOf(['0', 'xs', 'sm', 'md', 'lg', 'xl']),
+  mt: PropTypes.oneOf(['0', 'xs', 'sm', 'md', 'lg', 'xl']),
+  p: PropTypes.oneOf(['0', 'xs', 'sm', 'md', 'lg', 'xl']),
+  pb: PropTypes.oneOf(['0', 'xs', 'sm', 'md', 'lg', 'xl']),
+  pl: PropTypes.oneOf(['0', 'xs', 'sm', 'md', 'lg', 'xl']),
+  pr: PropTypes.oneOf(['0', 'xs', 'sm', 'md', 'lg', 'xl']),
+  pt: PropTypes.oneOf(['0', 'xs', 'sm', 'md', 'lg', 'xl']),
+  row: PropTypes.bool,
+  height: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl', 'full']),
+  width: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl', 'full']),
+  style: PropTypes.oneOf(['3-col', '4-col'])
+};
+Block.defaultProps = {
+  functions: {},
+  p: 'md'
+};
+
+var MainTopbar = function MainTopbar(_ref) {
+  var content = _ref.content,
+      functions = _ref.functions;
+  var LeftContent = content.LeftContent,
+      CenterContent = content.CenterContent,
+      RightContent = content.RightContent;
+  return /*#__PURE__*/React.createElement(Block, {
+    custom: "xp-topbar",
+    alignBetween: true,
+    alignMiddle: true,
+    bgColor: "primary-1",
+    p: "0",
+    row: true,
+    width: "full"
+  }, /*#__PURE__*/React.createElement(Block, null, LeftContent && /*#__PURE__*/React.createElement(LeftContent, {
+    functions: functions
+  })), /*#__PURE__*/React.createElement(Block, null, CenterContent && /*#__PURE__*/React.createElement(CenterContent, null)), /*#__PURE__*/React.createElement(Block, null, RightContent && /*#__PURE__*/React.createElement(RightContent, null)));
+};
+
+var LeftMenuIcon = function LeftMenuIcon(_ref2) {
+  var functions = _ref2.functions;
+  return /*#__PURE__*/React.createElement(Block, {
+    custom: "xp-menu-left-icon",
+    bgColor: "neutral-6",
+    functions: {
+      handleClick: functions.onLeftIcon
+    },
+    p: "sm"
+  }, "ICON");
+};
+
+var CenterContentMockup = function CenterContentMockup() {
+  return /*#__PURE__*/React.createElement(Block, {
+    custom: "xp-mockup",
+    bgColor: "neutral-4",
+    p: "sm"
+  }, "CenterContent");
+};
+
+var RightContentMockup = function RightContentMockup() {
+  return /*#__PURE__*/React.createElement(Block, {
+    custom: "xp-mockup",
+    bgColor: "neutral-6",
+    p: "sm"
+  }, "RightContent");
+};
+/* required props */
+
+
+LeftMenuIcon.propTypes = {
+  functions: PropTypes.objectOf(PropTypes.shape({
+    onLeftIcon: PropTypes.func
+  }))
+};
+MainTopbar.propTypes = {
+  content: PropTypes.object,
+  functions: PropTypes.objectOf(PropTypes.shape({
+    onLeftIcon: PropTypes.func
+  }))
+};
+MainTopbar.defaultProps = {
+  content: {
+    LeftContent: LeftMenuIcon,
+    CenterContent: CenterContentMockup,
+    RightContent: RightContentMockup
+  }
+};
+
+var MenuLeft = function MenuLeft(_ref) {
+  var functions = _ref.functions,
+      status = _ref.status;
+  var menuModalClasses = status.opened ? 'opened' : 'closed';
+  return /*#__PURE__*/React.createElement(Block, {
+    custom: "xp-menu-modal ".concat(menuModalClasses),
+    p: "0"
+  }, /*#__PURE__*/React.createElement(Block, {
+    custom: "xp-menu-opacity",
+    functions: {
+      handleClick: functions.onCloseModal
+    },
+    p: "0"
+  }), /*#__PURE__*/React.createElement(Block, {
+    custom: "xp-menu-left",
+    bgColor: "white",
+    p: "0"
+  }, "LeftContent===!284397q"));
+};
+/* required props */
+
+
+MenuLeft.propTypes = {
+  content: PropTypes.object,
+  functions: PropTypes.object,
+  status: {
+    opened: PropTypes.bool
+  }
+};
+MenuLeft.defaultProps = {
+  content: {},
+  functions: {
+    onCloseModal: function onCloseModal() {
+      return null;
+    }
+  },
+  status: {
+    opened: false
+  }
+};
+
+var index$1 = {
+  MenuLeft: MenuLeft,
+  MainTopbar: MainTopbar
+};
+
 var Panel = function Panel(_ref) {
   var children = _ref.children,
       style = _ref.style;
@@ -343,16 +367,8 @@ Panel.propTypes = {
   style: PropTypes.string
 };
 
-var version = "1.1.6";
-
-var Version = function Version() {
-  console.log('version ' + version);
-  return /*#__PURE__*/React.createElement("p", null, "version: ", version);
-};
-
 exports.Button = Button;
 exports.Form = index;
-exports.Frame = index$1;
 exports.Icon = Icon;
+exports.Navigator = index$1;
 exports.Panel = Panel;
-exports.Version = Version;
