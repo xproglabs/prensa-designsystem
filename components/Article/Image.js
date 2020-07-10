b6959a42701e22700aa360093f4b2d38966786a3
Fernando <fernando.bombassaro@gmail.com>
2020-07-10 18:30:57 -0300

    Adicionado imagem component

--- components/Article/Image.js
+++ components/Article/Image.js
@@ -1,21 +1,34 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const imageUrlBuilder = (policy, derivative, width) => 
-  `/image/${policy}/image.jpg?f=${derivative}&w=${width}`;
+import Block from '../Block';
 
 const Image = ({value}) => {
-  const imagePath = imageUrlBuilder(value, '2x1', 1000);
+
+  if(!value || !value['image-contentId'])
+    return false;
+
+  const contentid = value['image-contentId'];
+  const captionAndByline = value['image-subtitle'] ? 
+    `${value['image-subtitle']} (${value['image-byline']})` : 
+    `${value['image-subtitle-original']} (${value['image-byline']})`;
+    
+  const width = 1000;
+  const derivative = '2x1';
+  const imagePath = `/image/policy:${contentid}/image.jpg?f=${derivative}&w=${width}`;
+
   return (
-    <p>{imagePath}</p>
+    <Block custom="article-image-box" width="full">
+      <img className='image-article' src={imagePath} alt={captionAndByline ? captionAndByline : `Imagem ${contentid}`} />
+      <Block custom='label'>{captionAndByline}</Block>
+    </Block>
   );
 };
 
 Image.propTypes = {
-  value: PropTypes.string.isRequired
+  value: PropTypes.object.isRequired
 };
 Image.defaultProps = {
   value: {}
 };
-
 export default Image;
