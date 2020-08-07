9bdedd7075b765cbdd93633c3ebb0728c3e8823b
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-08-06 22:56:31 -0300

    Remove ícones teste

--- icons/index.js
+++ icons/index.js
@@ -1,4 +1,1 @@
-export { default as ArrowCircleDownBlack18Dp } from './ArrowCircleDownBlack18Dp'
-export { default as ArrowCircleUp24Px } from './ArrowCircleUp24Px'
-export { default as CompareArrows24Px } from './CompareArrows24Px'
-export { default as DoneOutline24Px } from './DoneOutline24Px'
+export {default as ArrowCircleDownBlack18Dp} from './ArrowCircleDownBlack18Dp';
