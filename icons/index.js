56bb6516f7437b1b8622749063b28741d67817ea
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-08-06 21:42:04 -0300

    Testando novo ícone

--- icons/index.js
+++ icons/index.js
@@ -1,3 +1,1 @@
-export {default as Artboard1} from './Artboard1';
-export {default as Artboard2} from './Artboard2';
-export {default as Accessibility24Px} from './Accessibility24Px';
+export { default as Contactless24Px } from './Contactless24Px'
