81d4aa27f572f89b23661f9d32801500007077de
Fernando <fernando.bombassaro@gmail.com>
2020-08-08 00:53:53 -0300

    Adicionado icon

--- icons/IcArrow.js
+++ icons/IcArrow.js
@@ -4,7 +4,10 @@ function SvgIcArrow(props) {
   return (
     <svg height={24} width={24} {...props}>
       <path d="M0 0h24v24H0V0z" fill="none" />
-      <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z" />
+      <path
+        className="ic-arrow_svg__arrow-item"
+        d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"
+      />
     </svg>
   );
 }
