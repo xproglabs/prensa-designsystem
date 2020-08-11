d8a97f769d74363a0587fdcab2409e6737c03afb
martinezpoa <mauricio@xproglabs.com>
2020-08-11 14:20:15 -0300

    ic forward

--- icons/IcArrowForward.js
+++ icons/IcArrowForward.js
@@ -2,7 +2,7 @@ import * as React from "react";
 
 function SvgIcArrowForward(props) {
   return (
-    <svg height={32} width={32} {...props}>
+    <svg height={32} viewBox="0 0 24 24" width={32} {...props}>
       <path d="M0 0h24v24H0z" fill="none" />
       <path d="M5.88 4.12L13.76 12l-7.88 7.88L8 22l10-10L8 2z" />
     </svg>
