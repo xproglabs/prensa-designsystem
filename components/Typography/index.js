ab705ae7f3191a3b6cd6fa8d408908faa1cfdfd3
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-09-19 17:54:53 -0300

    Ajustes finos no componente Typography e adiciona doc

--- components/Typography/index.js
+++ components/Typography/index.js
@@ -4,22 +4,6 @@ import React from 'react';
 
 const Typography = props => {
 
-  Typography.defaultProps = {
-    color: 'primary-1',
-    size: 1,
-    weight: 'regular',
-  };
-  
-  Typography.propTypes = {
-    children: PropTypes.string,
-    className: PropTypes.string,
-    color: PropTypes.oneOf(['primary-1', 'primary-2', 'primary-3']),
-    variant: PropTypes.oneOf(['subject', 'newsTitle', 'subtitle', 'date']),
-    size: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
-    style: PropTypes.object,
-    weight: PropTypes.oneOf(['thin', 'light', 'regular', 'medium', 'bold', 'italic'])
-  };
-
   const {
     className,
     children,
@@ -53,3 +37,19 @@ const Typography = props => {
 };
 
 export default Typography;
+
+Typography.defaultProps = {
+  color: 'primary-1',
+  size: 1,
+  weight: 'regular',
+};
+
+Typography.propTypes = {
+  children: PropTypes.string,
+  className: PropTypes.string,
+  color: PropTypes.oneOf(['primary-1', 'primary-2', 'primary-3']),
+  variant: PropTypes.oneOf(['subject', 'newsTitle', 'subtitle', 'date']),
+  size: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+  style: PropTypes.object,
+  weight: PropTypes.oneOf(['thin', 'light', 'regular', 'medium', 'bold', 'italic'])
+};
