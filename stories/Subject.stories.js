d00a388a6964d2a60063c00c7cce0fcc774b339c
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-09-21 15:08:11 -0300

    Atualiza o componente Subject

--- stories/Subject.stories.js
+++ stories/Subject.stories.js
@@ -1,29 +1,29 @@
-// import React from 'react';
+import React from 'react';
 
-// import Subject from '../components/Subject';
-// import {sassWrapper} from '../utils/SassWrapper';
+import Subject from '../components/Subject';
+import {sassWrapper} from '../utils/SassWrapper';
 
-// export default {
-//   title: 'Subject',
-//   component: Subject,
-//   decorators: [sassWrapper]
-// };
+export default {
+  title: 'Subject',
+  component: Subject,
+  decorators: [sassWrapper]
+};
 
-// export const Default = () => {
-//   return (
-//     <div style={{display: 'flex', flexDirection: 'column', padding: 40}}>
-//       <Subject>
-//         The quick fox
-//       </Subject>
-//     </div>
-//   );
-// };
-// export const Filled = () => {
-//   return (
-//     <div style={{display: 'flex', flexDirection: 'column', padding: 40}}>
-//       <Subject filled>
-//         The quick fox
-//       </Subject>
-//     </div>
-//   );
-// };
+export const Default = () => {
+  return (
+    <div style={{display: 'flex', flexDirection: 'column', padding: 40}}>
+      <Subject color='editorial-5'>
+        The quick fox
+      </Subject>
+      <Subject filled color='editorial-5'>
+        The quick fox
+      </Subject>
+      <Subject filled size={2} color='editorial-5'>
+        The quick fox
+      </Subject>
+      <Subject filled size={3} color='editorial-5'>
+        The quick fox
+      </Subject>
+    </div>
+  );
+};
