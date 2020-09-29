dca66bfedf34efbf6dbcd9de2890cd5d4efcdc66
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-09-29 16:56:06 -0300

    Adiciona max-width e props para o innerHtml no Paragraph

--- components/Typography/Paragraph.js
+++ components/Typography/Paragraph.js
@@ -9,20 +9,21 @@ const Paragraph = props => {
 
   const {
     className,
-    children,
     color,
     style,
     size,
-    weight
+    weight,
+    maxWidth,
+    setInnerHtml
   } = props;
 
   const classes = classnames({
     'Typography-Paragraph-root': true,
-    [`size-${size} ${weight} color-${color}`]: true,
+    [`size-${size} ${weight} color-${color} ${maxWidth}`]: true,
     [className]: className,
   });
 
-  return <p className={classes} style={style}>{children}</p>;
+  return <p className={classes} style={style} dangerouslySetInnerHTML={{__html: setInnerHtml}} />;
 };
 
 export default Paragraph;
@@ -30,14 +31,16 @@ export default Paragraph;
 Paragraph.defaultProps = {
   color: 'black',
   size: 1,
-  weight: 'regular'
+  weight: 'regular',
+  maxWidth: 'md'
 };
 
-Paragraph.propTypes = {
-  children: PropTypes.string,
+Paragraph.propTypes = {  
   className: PropTypes.string,
   color: PropTypes.oneOf(colors),
   size: PropTypes.oneOf([1]),
   style: PropTypes.object,
-  weight: PropTypes.oneOf(weight)
+  weight: PropTypes.oneOf(weight),
+  maxWidth: PropTypes.oneOf(['sm', 'md', 'lg']),
+  setInnerHtml: PropTypes.string,
 };
