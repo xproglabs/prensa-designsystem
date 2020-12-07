ada7c40e6c3f78c75acde0448e35c5d08ba6b281
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-12-07 19:58:16 -0300

    Adiciona ícone

--- icons/index.js
+++ icons/index.js
@@ -1,4 +1,5 @@
-export { default as ArrowCircleDownBlack18Dp } from './ArrowCircleDownBlack18Dp'
-export { default as IcArrowBack } from './IcArrowBack'
-export { default as IcArrowForward } from './IcArrowForward'
-export { default as IcArrowMenu } from './IcArrowMenu'
+export {default as ArrowCircleDownBlack18Dp} from './ArrowCircleDownBlack18Dp';
+export {default as IcArrowBack} from './IcArrowBack';
+export {default as IcArrowForward} from './IcArrowForward';
+export {default as IcArrowMenu} from './IcArrowMenu';
+export {default as IcSearch} from './IcSearch';
