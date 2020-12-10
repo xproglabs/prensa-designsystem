ac7e238314c5c14cda38c473bc978bdb1666be92
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-12-09 23:18:30 -0300

    Teste

--- components/Button/index.js
+++ components/Button/index.js
@@ -19,7 +19,6 @@ const Button = ({children, className, color, disabled, fontColor, fullWidth, lef
   });
 
   const getFontColor = () => {
-    console.log('fontCOlor');
     if (fontColor) return fontColor;
     if (variant === 'outlined') {
       if (disabled) return 'neutral-8';
