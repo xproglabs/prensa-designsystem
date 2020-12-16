7035d218f46f04401490131659b8fb2e17840da9
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-12-16 11:17:06 -0300

    Adiciona bloco RelatedContent

---
+++ components/Templates/RelatedContent.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import RelatedContent from './RelatedContent';
+
+export default {
+  title: 'RelatedContent',
+  component: RelatedContent,
+};
+
+export const Component = () => {
+  return (
+    <RelatedContent />
+  );
+};
