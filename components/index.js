c0a83220505881821cf97cefc817c589cad4b031
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-01-07 23:08:24 -0300

    Remove componentes de Menu antigos e atualiza changelog

--- components/index.js
+++ components/index.js
@@ -6,13 +6,10 @@ export {default as Field} from './Field';
 export {default as Image} from './Image';
 export {default as ImageBackground} from './Image/Background';
 export {default as ImageGallery} from './ImageGallery';
-export {default as SearchForm} from './Menus/SearchForm';
-export {default as SearchMenu} from './Menus/Search';
-export {default as SideMenu} from './Menus/SideMenu';
-export {default as SideMenuItems} from './Menus/SideItems';
+export {default as SideMenu} from './SideMenu';
 export {default as SocialMedias} from './SocialMedias';
 export {default as Topbar} from './Topbar';
 export {default as Teaser} from './Teasers';
-export {theme} from '../styles/theme'
+export {theme} from '../styles/theme';
 export {default as Subject} from './Subject';
 export {default as Tag} from './Tag';
