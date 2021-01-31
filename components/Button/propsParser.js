e4523e7f5882a8b1d1f678e9f013a4455ef28b6b
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-01-31 20:08:48 -0300

    Ajustes finos

--- components/Button/propsParser.js
+++ components/Button/propsParser.js
@@ -26,7 +26,7 @@ const getOutlinedCSS = (props, theme) => `
   background-color: transparent;
   border-width: 1px;
   border-style: solid;
-  border-color: ${theme.parseColorValue(props, theme, '$color')};
+  border-color: ${theme.parseColorValue(props, '$color')};
   &:disabled {
     border-color: ${theme.colors.neutral8};
   }
@@ -35,7 +35,7 @@ const getGhostCSS = () => `
   background-color: transparent;
 `;
 const getFilledCSS = (props, theme) =>`
-  background-color: ${theme.parseColorValue(props, theme, '$color')};
+  background-color: ${theme.parseColorValue(props, '$color')};
   &:disabled {
     background-color: ${theme.colors.neutral8};
   }
@@ -52,8 +52,8 @@ const parseVariation = (props, theme) => {
 const parseFontColor = (props, theme) => {
   const {fontColor, $variant, disabled} = props;
   if (disabled && $variant !== 'filled') return theme.colors.neutral8;
-  if (fontColor) return theme.parseColorValue(props, theme, 'fontColor');
-  if ($variant === 'outlined' || $variant === 'ghost') return theme.parseColorValue(props, theme, '$color');
+  if (fontColor) return theme.parseColorValue(props, 'fontColor');
+  if ($variant === 'outlined' || $variant === 'ghost') return theme.parseColorValue(props, '$color');
   return theme.colors.white;
 };
 const parseFontFamily = (props, theme) => {
--- styles/parsers/parseColors.js
+++ styles/parsers/parseColors.js
@@ -1,24 +1,24 @@
 import {get} from 'lodash';
 
 const parseBgColor = (props, theme) => {
-  if (!props) return ''; 
+  if (!props || !props.bgColor) return ''; 
   const selected = theme.colors[props.bgColor];
   if (!selected) return '';
   return `background-color: ${selected};`;
 };
 
 const parseFontColor = (props, theme) => {
-  if (!props) return '';
+  if (!props || !props.fontColor) return '';
   const selected = theme.colors[props.fontColor];
   if (!selected) return '';
   return `color: ${selected};`;
 };
 
-const parseColorValue = (props, theme, propName) => {
+const parseColorValue = (props, propName) => {
   if (!propName) return console.error('PRENSA :: Não existe nome de prop para buscar no parseColor');
   const propValue = get(props, propName, 'activeColor');
   if (propValue.startsWith('#')) return propValue;
-  const parsedValue = theme.colors[propValue];
+  const parsedValue = props.theme.colors[propValue];
   if (!parsedValue) return console.error(`PRENSA :: Não existe valor correspondente para a prop de nome ${propName} passada na função parseColor`);
   return parsedValue;
 };
