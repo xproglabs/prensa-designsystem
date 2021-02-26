da1737be0f69b800eab273efb928fcae14954eae
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-02-25 22:01:00 -0300

    Adiciona queries

--- components/Typography/index.js
+++ components/Typography/index.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled, {css} from 'styled-components';
+import styled, {css, withTheme} from 'styled-components';
 
 import DynamicComponent from '../DynamicComponent';
-import {parseFontFamily, parseSize} from './parsers';
+import {parseProps} from './parsers';
 
 const Typography = ({
   children,
@@ -15,16 +15,64 @@ const Typography = ({
   mt,
   mb,
   ml,
-  mr
+  mr,
+  xs,
+  sm,
+  md,
+  lg,
+  xl,
+  theme
 }) => {
 
   const styles = css`
-    font-weight: ${fontWeight};
-    font-family: ${parseFontFamily};
-    font-size: ${props => parseSize(props, 'fontSize')};
-    line-height: ${props => parseSize(props, 'lineHeight')};
-    ${props => props.theme.parseMargin(props, props.theme)};
+    ${props => parseProps('', props)};
+    ${props => props.xs && parseProps('xs', props)};
+    ${props => props.sm && parseProps('sm', props)};
+    ${props => props.md && parseProps('md', props)};
+    ${props => props.lg && parseProps('lg', props)};
+    ${props => props.xl && parseProps('xl', props)};
   `;
+
+  const getXsProps = () => xs && ({
+    ...xs,
+    theme,
+    $fontWeight: xs.fontWeight ? xs.fontWeight : fontWeight,
+    $fontSize: xs.fontSize ? xs.fontSize : fontSize,
+    $fontFamily: xs.fontFamily ? xs.fontFamily : fontFamily,
+    $lineHeight: xs.lineHeight ? xs.lineHeight : lineHeight,
+  });
+  const getSmProps = () => sm && ({
+    ...sm,
+    theme,
+    $fontWeight: sm.fontWeight ? sm.fontWeight : fontWeight,
+    $fontSize: sm.fontSize ? sm.fontSize : fontSize,
+    $fontFamily: sm.fontFamily ? sm.fontFamily : fontFamily,
+    $lineHeight: sm.lineHeight ? sm.lineHeight : lineHeight,
+  });
+  const getMdProps = () => md && ({
+    ...md,
+    theme,
+    $fontWeight: md.fontWeight ? md.fontWeight : fontWeight,
+    $fontSize: md.fontSize ? md.fontSize : fontSize,
+    $fontFamily: md.fontFamily ? md.fontFamily : fontFamily,
+    $lineHeight: md.lineHeight ? md.lineHeight : lineHeight,
+  });
+  const getLgProps = () => lg && ({
+    ...lg,
+    theme,
+    $fontWeight: lg.fontWeight ? lg.fontWeight : fontWeight,
+    $fontSize: lg.fontSize ? lg.fontSize : fontSize,
+    $fontFamily: lg.fontFamily ? lg.fontFamily : fontFamily,
+    $lineHeight: lg.lineHeight ? lg.lineHeight : lineHeight,
+  });
+  const getXlProps = () => xl && ({
+    ...xl,
+    theme,
+    $fontWeight: xl.fontWeight ? xl.fontWeight : fontWeight,
+    $fontSize: xl.fontSize ? xl.fontSize : fontSize,
+    $fontFamily: xl.fontFamily ? xl.fontFamily : fontFamily,
+    $lineHeight: xl.lineHeight ? xl.lineHeight : lineHeight,
+  });
 
   const props = {
     $fontWeight: fontWeight,
@@ -34,7 +82,12 @@ const Typography = ({
     mt,
     mb,
     ml,
-    mr
+    mr,
+    xs: getXsProps(),
+    sm: getSmProps(),
+    md: getMdProps(),
+    lg: getLgProps(),
+    xl: getXlProps(),
   };
 
   const StyledTypography = styled(DynamicComponent).attrs({element})`${styles}`;
@@ -68,7 +121,15 @@ Typography.propTypes = {
   mr: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   mb: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   ml: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /**
+   * Propriedades responsivas
+   */
+  xs: PropTypes.object,
+  sm: PropTypes.object,
+  md: PropTypes.object,
+  lg: PropTypes.object,
+  xl: PropTypes.object,
   theme: PropTypes.object
 };
 
-export default Typography;
+export default withTheme(Typography);
--- components/Typography/parsers.js
+++ components/Typography/parsers.js
@@ -1,16 +1,63 @@
 import {get} from 'lodash';
 
-export const parseFontFamily = props => {
+const parseFontFamily = props => {
   const fonts = get(props, 'theme.fonts', {});
   const propValue = get(props, '$fontFamily', '');
   const selected = get(fonts, propValue, '');
   return selected;
 };
 
-export const parseSize = (props, propName) => {
-  const factor = get(props, 'theme.factors.dimensions', {});
+const parseSize = (props, propName) => {
+  const factor = get(props, 'theme.factors.margin', {});
   const propValue = get(props, `$${propName}`);
   const isNumber = typeof propValue === 'number';
   if (isNumber) return `${propValue * factor}px`;
   return propValue;
+};
+
+const parseStyle = (props, theme) => {
+  return `
+    font-weight: ${props.$fontWeight};
+    font-family: ${parseFontFamily(props)};
+    font-size: ${parseSize(props, 'fontSize')};
+    line-height: ${parseSize(props, 'lineHeight')};
+    ${theme.parseMargin(props, theme)};
+  `;
+};
+
+export const parseProps = (media, props) => {
+  switch (media) {
+    case 'xs':
+      return `
+        @media (min-width: ${props.theme.queries.xs}) {
+          ${parseStyle(props.xs, props.theme)}
+        }
+      `;
+    case 'sm':
+      return `
+        @media (min-width: ${props.theme.queries.sm}) {
+          ${parseStyle(props.sm, props.theme)}
+        }
+      `;
+    case 'md':
+      return `
+        @media (min-width: ${props.theme.queries.md}) {
+          ${parseStyle(props.md, props.theme)}
+        }
+      `;
+    case 'lg':
+      return `
+        @media (min-width: ${props.theme.queries.lg}) {
+          ${parseStyle(props.lg, props.theme)}
+        }
+      `;
+    case 'xl':
+      return `
+        @media (min-width: ${props.theme.queries.xl}) {
+          ${parseStyle(props.xl, props.theme)}
+        }
+      `;
+    default:
+      return `${parseStyle(props, props.theme)}`;
+  }
 };
