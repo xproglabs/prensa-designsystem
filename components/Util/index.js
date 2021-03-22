ce006f498de7e439870a40d66e5b15883a3060cf
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-03-22 14:42:24 -0300

    Tentativa de corrigir problema na build

--- components/Util/index.js
+++ components/Util/index.js
@@ -1,2 +1,4 @@
 export {default as dateDistance} from './dateDistance';
 export {default as datePtBrFull} from './datePtBrFull';
+export {default as numberToString} from './numberToString';
+export {default as getFromProps} from './getFromProps';
