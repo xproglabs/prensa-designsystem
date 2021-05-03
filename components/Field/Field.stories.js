0cc3bd06e526dd239387cec71390b70701476c03
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-05-03 20:12:59 -0300

    Novo boilerplate ScrollIntoView com Field

--- components/Field/Field.stories.js
+++ components/Field/Field.stories.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {ThemeProvider} from 'styled-components';
 
 import {theme} from '../../styles/theme';
@@ -9,9 +9,10 @@ export default {
   component: Field,
 };
 
-export const Primary = () => {
+export const Default = () => {
   const [state, setState] = React.useState('');
   const [text, setText] = React.useState('');
+
   return (
     <ThemeProvider theme={theme}>
       <div style={{margin: 20}}>
@@ -22,15 +23,22 @@ export const Primary = () => {
   );
 };
 
-Primary.args = {
-  children: 'Avançar',
-};
+export const ScrollIntoView = () => {
+  const [state, setState] = React.useState('');
+  const [text, setText] = React.useState('');
+  const fieldRef = useRef();
 
-Primary.argTypes = {
-  color: {
-    control: {
-      type: 'select',
-      options: ['primary', 'secondary']
-    }
-  }
+  const scrollToField = () => {
+    fieldRef.current.scrollIntoView({behavior: 'smooth', block: 'center'});
+  };
+
+  return (
+    <ThemeProvider theme={theme}>      
+      <div style={{margin: 20, height: 3000, border: '1px solid black'}}>
+        <Field ref={fieldRef} placeholder="Digite alguma coisa" onEnterKey={() => setText('click enter key')} value={state} onChange={setState} validation={false} validationMessage="Teste"/>
+        <pre>{text}</pre>
+      </div>
+      <button onClick={scrollToField}>scroll to field</button>
+    </ThemeProvider>
+  );
 };
