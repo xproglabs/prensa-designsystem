625c4de0488bf3433c68f294061d9b4347157596
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-06-05 20:29:51 -0300

    Replica o ImageGallery na pasta destino

--- components/ImageGallery/index.js
+++ components/ImageGallery/index.js
@@ -2,65 +2,63 @@ import {map} from 'lodash';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import SvgIcArrowBack from '../../icons/IcArrowBack';
-import SvgIcArrowForward from '../../icons/IcArrowForward';
-import Block from '../Block';
+import {Container, Subtitle, SubtitleBox} from './styled';
 
-const RenderItem = ({linkurl, name, lead, selected, on}) => {
-  const propsGallerySelected = {custom: 'gallery-selected', w: '100p', mb: '2'};
-  const propsGalleryItem = {custom: 'gallery-item', mr: '2', onClick: () => on({linkurl, name, lead})};
-  const propsGallery = selected ? propsGallerySelected : propsGalleryItem;
+const ImageGallery = ({caption, items, height, width}) => {
   return (
-    <Block {...propsGallery}>
-      <img className='image-file' src={linkurl} alt={`${name}`} />
-      <Block custom='image-name'>{name}</Block>
-      <Block custom='image-lead'>{lead}</Block>
-    </Block>
+    <Container $height={height} $width={width}>
+      <amp-carousel
+        lightbox={true}
+        height="420"
+        layout="responsive"
+        type="slides"
+        width="720">
+        {map(items, (item, key) => (
+          <figure>
+            <amp-img 
+              alt={item.caption}
+              src={item.value}
+              layout="responsive"
+              height="360"
+              key={key}
+              width="720"
+            />
+            <figcaption>    
+              <SubtitleBox>
+                <Subtitle
+                  fontFamily={caption.fontFamily}
+                  fontSize={caption.fontSize}
+                  lineHeight={caption.lineHeight}>
+                  {item.caption}
+                </Subtitle>
+              </SubtitleBox>
+            </figcaption>
+          </figure>
+        ))}
+      </amp-carousel>
+    </Container>
   );
 };
 
-const ImageGallery = ({items, selected, on}) => {
-  if(!items || items.length === 0) return false;
-  const item_selected = selected ? selected : items[0];
+export const gallery_amp_script = () => (
+  <script async custom-element="amp-lightbox-gallery" src="https://cdn.ampproject.org/v0/amp-lightbox-gallery-0.1.js" />
+);
 
-  return (
-    <Block custom="article-image-gallery" w="100p">
-      <Block align="row" custom="image-selected" w="100p">
-        <RenderItem {...item_selected} selected on={on} />
-      </Block>
-      {(items.length > 1) && (
-        <Block align="row" custom="box-list" w="100p">
-          <Block align="row center middle" custom="arrow-left"><SvgIcArrowBack /></Block>
-          <Block align="row" custom="image-list" w="100p">
-            {map(items, (item, key) => <RenderItem {...item} key={key} on={on} />)}
-          </Block>
-          <Block align="row center middle" custom="arrow-right"><SvgIcArrowForward /></Block>
-        </Block>
-      )}
-    </Block>
-  );
+ImageGallery.defaultProps = {
+  caption: {
+    fontFamily: 'secondary',
+    fontSize: ['14px', '14px'],
+    lineHeight: ['130%', '130%']
+  },
+  height: '420px',
+  width: '720px'
 };
 
 ImageGallery.propTypes = {
+  caption: PropTypes.object,
   items: PropTypes.array,
-  selected: PropTypes.shape({
-    lead: PropTypes.string,
-    linkUrl: PropTypes.string,
-    name: PropTypes.string,
-  }),
-  on: PropTypes.func,  
+  height: PropTypes.string,
+  width: PropTypes.string,
 };
 
-RenderItem.propTypes = {
-  linkurl: PropTypes.string,
-  name: PropTypes.string,
-  lead: PropTypes.string,
-  selected: PropTypes.bool,
-  on: PropTypes.func.isRequired
-};
-
-ImageGallery.defaultProps = {
-  selected: false,
-  items: []
-};
 export default ImageGallery;
