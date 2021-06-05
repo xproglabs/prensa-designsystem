efedfb950a2506a4d6d58f8523e1ee025d6a37ba
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-06-05 20:21:36 -0300

    Adiciona o componente de galeria estilizado

---
+++ prensa-app/components/ImageGallery/index.js
@@ -0,0 +1,64 @@
+import {map} from 'lodash';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import {Container, Subtitle, SubtitleBox} from './styled';
+
+const ImageGallery = ({caption, items, height, width}) => {
+  return (
+    <Container $height={height} $width={width}>
+      <amp-carousel
+        lightbox={true}
+        height="420"
+        layout="responsive"
+        type="slides"
+        width="720">
+        {map(items, (item, key) => (
+          <figure>
+            <amp-img 
+              alt={item.caption}
+              src={item.value}
+              layout="responsive"
+              height="360"
+              key={key}
+              width="720"
+            />
+            <figcaption>    
+              <SubtitleBox>
+                <Subtitle
+                  fontFamily={caption.fontFamily}
+                  fontSize={caption.fontSize}
+                  lineHeight={caption.lineHeight}>
+                  {item.caption}
+                </Subtitle>
+              </SubtitleBox>
+            </figcaption>
+          </figure>
+        ))}
+      </amp-carousel>
+    </Container>
+  );
+};
+
+export const gallery_amp_script = () => (
+  <script async custom-element="amp-lightbox-gallery" src="https://cdn.ampproject.org/v0/amp-lightbox-gallery-0.1.js" />
+);
+
+ImageGallery.defaultProps = {
+  caption: {
+    fontFamily: 'secondary',
+    fontSize: ['14px', '14px'],
+    lineHeight: ['130%', '130%']
+  },
+  height: '420px',
+  width: '720px'
+};
+
+ImageGallery.propTypes = {
+  caption: PropTypes.object,
+  items: PropTypes.array,
+  height: PropTypes.string,
+  width: PropTypes.string,
+};
+
+export default ImageGallery;
--- prensa-app/pages/index.js
+++ prensa-app/pages/index.js
@@ -1,17 +1,18 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import styled from 'styled-components'
-import React from 'react'
+import Head from 'next/head';
+import React from 'react';
+import styled from 'styled-components';
 
-import TwitterEmbed, {twitter_amp_script} from '../components/embeds/Twitter'
-import InstagramEmbed, {instagram_amp_script} from '../components/embeds/Instagram'
-import FacebookEmbed, {facebook_amp_script} from '../components/embeds/Facebook'
-import YouTubeEmbed, {youtube_amp_script} from '../components/embeds/YouTube'
+import FacebookEmbed, {facebook_amp_script} from '../components/embeds/Facebook';
+import InstagramEmbed, {instagram_amp_script} from '../components/embeds/Instagram';
+import TwitterEmbed, {twitter_amp_script} from '../components/embeds/Twitter';
+import YouTubeEmbed, {youtube_amp_script} from '../components/embeds/YouTube';
+import ImageGallery from '../components/ImageGallery';
+import styles from '../styles/Home.module.css';
 
 const H1 = styled.h1`
   font-family: inherit;
   color: red;
-`
+`;
 const Main = styled.main`
   padding: 5rem 1rem;
   flex: 1;
@@ -19,18 +20,32 @@ const Main = styled.main`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
 
 export const config = {amp: true};
 
 export default function Home() {
-  
-  // https://twitter.com/CNNBrasil/status/1242733524710387713?s=20
-  const tweetId = '1242733524710387713'
-  const instagramUrl = 'https://www.instagram.com/p/Bz4H4mahyOq'
-  const facebookUrl = 'https://www.facebook.com/HugoGloss/posts/10158171335166146'
-  const youtubeUrl = 'https://www.youtube.com/watch?v=qJzT_Ww8q9g'
-
+  const tweetId = '1242733524710387713';
+  const instagramUrl = 'https://www.instagram.com/p/Bz4H4mahyOq';
+  const facebookUrl = 'https://www.facebook.com/HugoGloss/posts/10158171335166146';
+  const youtubeUrl = 'https://www.youtube.com/watch?v=qJzT_Ww8q9g';
+  const imageGallery = [
+    {
+      caption: 'Mariane criou empresa com foco na economia sustentável (Fotos: Divulgação)',
+      contentId: '1.3658',
+      value: 'http://qa.diariodaregiao.com.br/image/policy:1.3658/image.jpg?f=2x1&w=1200'
+    },
+    {
+      caption: 'O cabeleireiro Douglas Negrett montou salão para valorizar estética negra (Guilherme Baffi 7/5/2021)',
+      contentId: '1.34636',
+      value: 'http://qa.diariodaregiao.com.br/image/policy:1.34636/image.jpg?f=2x1&w=1200'
+    },
+    {
+      caption: 'Isadora, filha da médica Luciana Casseb, com camiseta da marca ',
+      contentId: '1.34723',
+      value: 'http://qa.diariodaregiao.com.br/image/policy:1.34723/image.jpg?f=2x1&w=1200'
+    }
+  ];
   return (
     <div className={styles.container}>
       <Head>
@@ -45,11 +60,12 @@ export default function Home() {
 
       <Main>
         <H1>Componente aqui</H1>
+        <ImageGallery items={imageGallery} />
         <TwitterEmbed tweetId={tweetId} />
         <InstagramEmbed url={instagramUrl} />
         <FacebookEmbed url={facebookUrl} />
         <YouTubeEmbed url={youtubeUrl} />
       </Main>
     </div>
-  )
+  );
 }
