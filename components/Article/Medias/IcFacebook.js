0e90df6ca44c8cd9c56b9e15adc91faa51623792
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-06-06 22:54:25 -0300

    Atualiza ícones

--- components/Article/Medias/IcFacebook.js
+++ components/Article/Medias/IcFacebook.js
@@ -1,16 +1,26 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-const SvgIcFacebook = () => {
+import {IconContainer} from './styled';
+
+const IcFaceboook = ({color, height, width}) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-      <g id="Facebook" transform="translate(-212.261 -245.173)">
-        <g id="Grupo_1690" data-name="Grupo 1690" transform="translate(212.261 245.173)">
-          <path id="Caminho_301" data-name="Caminho 301" d="M244.261,261.174a16,16,0,1,1-16-16A16,16,0,0,1,244.261,261.174Z" transform="translate(-212.261 -245.173)" fill=""/>
-          <path id="Caminho_302" className="face" data-name="Caminho 302" d="M230.776,255.407h-2.848v-1.868a.759.759,0,0,1,.792-.864h2.01V249.59l-2.768-.011a3.506,3.506,0,0,0-3.773,3.773v2.055h-1.777v3.178h1.777v8.994h3.739v-8.994h2.522l.325-3.178Z" transform="translate(-210.683 -242.922)" fill="#1769ac"/>
-        </g>
-      </g>
-    </svg>
+    <IconContainer xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" $color={color} $height={height} $width={width}>
+      <path d="M22.23,4.15H19.16c-3.45,0-5.69,2.29-5.69,5.83v2.69H10.38a.49.49,0,0,0-.48.48v3.9a.49.49,0,0,0,.48.49h3.09v9.83a.48.48,0,0,0,.49.48h4a.47.47,0,0,0,.48-.48V17.54h3.62a.49.49,0,0,0,.48-.49v-3.9a.49.49,0,0,0-.14-.34.48.48,0,0,0-.34-.14H18.47V10.39c0-1.1.26-1.65,1.69-1.65h2.07a.49.49,0,0,0,.49-.49V4.63A.49.49,0,0,0,22.23,4.15Z"/>
+    </IconContainer>
   );
 };
 
-export default SvgIcFacebook;
+IcFaceboook.defaultProps = {
+  width: '24',
+  height: '24',
+  color: 'primary1'
+};
+
+IcFaceboook.propTypes = {
+  color: PropTypes.string,
+  height: PropTypes.string,
+  width: PropTypes.string
+};
+
+export default IcFaceboook;
