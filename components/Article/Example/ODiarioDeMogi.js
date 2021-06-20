8f466411d5ed37a2bc2a01fd9fc5c7e31c1299e7
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-06-20 04:24:09 -0300

    Implementa o novo article no omogi

--- components/Article/Example/ODiarioDeMogi.js
+++ components/Article/Example/ODiarioDeMogi.js
@@ -1,68 +1,206 @@
+import {map} from 'lodash';
+import PropTypes from 'prop-types';
 import React from 'react';
-import {ThemeProvider} from 'styled-components';
 
 import {theme as odiariodemogi} from '../../../styles/demo/odiariodemogi';
+import Block from '../../Block';
+import datePtBrFull from '../../Util/datePtBrFull';
+import {pathToImage} from '../../Util/pathToImage';
 import Article from '../Article';
+import WithThemeProvider from '../WithThemeProvider';
+import article from './ODiarioDeMogi.json';
 
 const ArticleOM = () => {
+  const domain = 'http://localhost:8080';
+  const {
+    author,
+    contentId,
+    content,
+    gallery,
+    image: images,
+    keymetas,
+    keywords,
+    metadata,
+    multimidia,
+    options,
+    path,
+    priority,
+    template,
+    texts,
+    timestamps,
+    title,
+    subject,
+    subtitle,
+    subtitle_image,
+    video_url
+  } = article;
+  // configura o topimage
+  const isAmp = false; // useAmp();
+  const isFeatured = false; // config.article_is_featured;
+  const config = {
+    header_color: 'primary1'
+  };
+  const topimage = {
+    featured: isFeatured,
+    image: true,
+    caption: {
+      fontFamily: 'secondary',
+      fontSize: ['14px', '14px'],
+      lineHeight: ['130%', '130%'],
+      px: ['3', '4'],
+      py: ['3', '4'],
+      show: true,
+      value: ''
+    },
+    mb: isFeatured ? ['0px', '0px'] : ['5', '8'],
+    value: ''
+  };
+  // verifica se tem imagem
+  if (images && images.length > 0) {
+    const image_one = images[0];
+    const image_id = image_one.contentId;
+    topimage.value = pathToImage('2x1', domain, image_id, 1200);
+    topimage.caption.value = `${subtitle_image || image_one.caption} ${image_one.byline && `(${image_one.byline})`}`;
+  } else {
+    topimage.image = false;
+  }
+  let images_parsed = [];
+  map(images, (img) => {
+    let img_path = pathToImage('2x1', domain, img.contentId, 1200)
+    let img_caption = `${img.caption} ${img.byline && `(${img.byline})`}`;
+    images_parsed.push({
+      caption: img_caption,
+      contentId: img.contentId,
+      value: img_path
+    });
+  });
+  let gallery_parsed = [];
+  map(gallery, (img) => {
+    let img_path = pathToImage('2x1', domain, img.contentId, 1200)
+    let img_caption = `${img.caption} ${img.byline && `(${img.byline})`}`;
+    gallery_parsed.push({
+      caption: img_caption,
+      contentId: img.contentId,
+      value: img_path
+    });
+  });
+  const time_modified = timestamps ? datePtBrFull(timestamps.time_modified) : '';
+  const time_published = timestamps ? datePtBrFull(timestamps.time_published) : '';
+  let content_body = content;
+  map(multimidia, (midia) => {
+    if(midia.template == 'br.com.atex.plugins.youtube.it') {
+      content_body = `${content_body}\n\n<p><a id="${midia.link}" name="${midia.link}">${midia.link}</a></p>`;
+    }
+  });
+  map(texts, (text) => {
+    let text_images = ''
+    map(text.image, (im) => {
+      let img_path = pathToImage('2x1', domain, im.contentId, 1200)
+      text_images = `${text_images}<p><a class="p-smartembed" data-attr-f="2x1" data-attr-q="1" data-attr-w="1000" data-onecms-id="policy:${im.contentId}" data-onecms-type="image" href="javascript:window.parent.actionEventData({$contentId:'${im.contentId}', $action:'view', $target:'work'})" polopoly:contentid="policy:${im.contentId}"><img src="/image/policy:${contentId}/image.jpg?f=2x1&amp;w=1000&amp;q=1" /></a></p>\r\n\r\n`;
+      images_parsed.push({
+        caption: im.caption,
+        contentId: im.contentId,
+        value: img_path
+      });
+    });
+    content_body = `
+      ${content_body}\n\n
+      ${text.title && (`<h3>${text.title}</h3>\n\n`)}
+      ${text_images}\n\n
+      ${text.content}`;
+  });
+  const byline = {
+    author: {
+      color: 'neutral4',
+      fontFamily: 'primary',
+      fontSize: ['14px', '14px'],
+      lineHeight: ['16px', '16px'],
+      value: author
+    },
+    customContent: `
+      border-left: 8px solid ${odiariodemogi.colors['primary1']};
+      padding-left: 8px;
+    `,
+    datetime: {
+      color: 'neutral4',
+      fontSize: ['12px', '12px'],
+      lineHeight: ['14px', '14px'],
+      time_modified,
+      time_published
+    }
+  };
+  const intertitle = {
+    fontSize: ['38px', '30px'],
+    fontWeight: 700,
+    mb: ['3', '5'],
+    value: 'Título'
+  };
+  const props_subject = {
+    bgColor: config.header_color,
+    color: 'white',
+    mb: ['3', '3'],
+    transform: 'uppercase',
+    value: subject
+  };
+  const props_subtitle = {
+    color: 'neutral3',
+    mb: ['5', '5'],
+    value: subtitle
+  };
+  const props_title = {
+    color: 'neutral2',
+    fontSize: ['38px', '62px'],
+    lineHeight: ['42px', '64px'],
+    mb: ['2', '2'],
+    value: title
+  };
+  const props_paragraph = {
+    fontFamily: 'secondary',
+    fontSize: ['20px', '20px'],
+    lineHeight: ['190%', '190%'],
+  };
+  const RenderArticle = ({paywallNotSubscriber}) => 
+    <Article
+      amp={isAmp}
+      byline={byline}
+      featured={{enabled: isFeatured}}
+      gallery={gallery_parsed}
+      images={{items: images_parsed}}
+      intertitle={intertitle}
+      subject={props_subject}
+      subtitle={props_subtitle}
+      tags={{items: metadata}}
+      textbody={paywallNotSubscriber ? '' : content_body}
+      title={props_title}
+      paragraph={props_paragraph}
+      topimage={topimage}
+    />;
+
+  RenderArticle.propTypes = {
+    paywallNotSubscriber: PropTypes.bool
+  };
+
+  if(priority == '1') {
+    return (
+      <WithThemeProvider theme={odiariodemogi}>
+        <Block alignx='center' width='100%'>
+          <div amp-access="NOT subscriber" amp-access-hide="true">
+            <RenderArticle paywallNotSubscriber={true} />
+            {/* <PayBlock callback={path} /> */}
+          </div>
+          <div amp-access="loggedIn AND subscriber" amp-access-hide="true">
+            <RenderArticle />
+          </div>
+        </Block>
+      </WithThemeProvider>
+    );
+  }
   return (
-    <ThemeProvider theme={odiariodemogi}>      
-      <Article
-        byline={{
-          customContent: `
-            border-left: 8px solid ${odiariodemogi.colors['primary1']};
-            padding-left: 8px;
-          `,
-          datetime: {
-            color: 'neutral4',
-            fontSize: ['14px', '14px'],
-            lineHeight: ['16px', '16px'],
-            time_modified: '18 dias atrás',
-            time_published: '21/05/2021 às 23:20',
-          }
-        }}
-        intertitle={{
-          fontSize: ['38px', '30px'],
-          value: 'Título interno da matéria'
-        }}
-        paragraph={{
-          fontFamily: 'secondary',
-          fontSize: ['20px', '20px'],
-          lineHeight: ['190%', '190%'],
-        }}
-        subject={{
-          mb: ['3', '3'],
-          transform: 'uppercase',
-          value: 'O Diário de Mogi',
-        }}
-        subtitle={{
-          mb: ['5', '5'],
-          value: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra eleifend pellentesque. Suspendisse semper gravida fermentum. Integer pulvinar euismod hendrerit.'
-        }}
-        tags={{
-          items: ['Tag1', 'Tag2']
-        }}
-        textbody='<p>Deputada estadual pelo Partido dos Trabalhadores (PT) desde 2003, Beth Sah&atilde;o, uma das maiores lideran&ccedil;as do partido na regi&atilde;o, escondeu o ex-presidente Luis In&aacute;cio Lula da Silva e seu partido em sua primeira propaganda eleitoral para a TV na disputa pela prefeitura de Catanduva.</p><p>A situa&ccedil;&atilde;o j&aacute; tem ocorrido nas redes sociais, cen&aacute;rio em que Beth aparece com as cores azul, branco e laranja em sua campanha. A candidata a prefeita, que disputa a vaga do Executivo da cidade pela quarta vez, tamb&eacute;m reduziu o n&uacute;mero 13, que sempre esteve em evid&ecirc;ncia, nas apari&ccedil;&otilde;es. Este ano, o n&uacute;mero surge menor e grafado em branco.</p><p>Grande marca do partido e tamb&eacute;m j&aacute; utilizada em outras campanhas de Beth, a estrela do PT, s&iacute;mbolo nacional da sigla, tamb&eacute;m n&atilde;o aparece na campanha da ex-deputada, que deixou a condi&ccedil;&atilde;o de parlamentar em agosto ap&oacute;s o titular reaver o mandato. Igualmente preteridos, os partidos da coliga&ccedil;&atilde;o (PT/PTB/SD/Avante/PSB/PSD/PROS) surgiram sem destaque nas imagens de divulga&ccedil;&atilde;o.</p><p>Em 2016, quando Beth ficou em segundo lugar, contra Afonso Macchione (PSB), a ex-parlamentar tamb&eacute;m j&aacute; vinha se afastando da cor vermelha e outros elementos do PT. No entanto, Beth ainda mantinha em evid&ecirc;ncia o n&uacute;mero 13 e a estrela. Outro detalhe que chama aten&ccedil;&atilde;o na campanha da petista &eacute; a presen&ccedil;a do PSB, partido pelo qual Macchione disputou as &uacute;ltimas elei&ccedil;&otilde;es, al&eacute;m do PSD de Gilberto Kassab, que atualmente est&aacute; aliado ao PSDB e ao DEM, rivais hist&oacute;ricos do PT.</p><p>A reportagem procurou Beth, que por meio de sua assessoria disse que Lula vota em S&atilde;o Bernardo e ela &eacute; candidata em Catanduva. Em rela&ccedil;&atilde;o &agrave;s cores da campanha, a prefeitur&aacute;vel disse que o azul e o laranja "propiciaram uma composi&ccedil;&atilde;o mais harmoniosa em termos est&eacute;ticos".</p><p><strong>Em Rio Preto</strong></p><p>Por sua vez, a candidata a prefeita de Rio Preto pelo PT, Celi Regina, contou com a inser&ccedil;&atilde;o do ex-presidente Lula e do ex-ministro Fernando Haddad como trunfos para sua propaganda eleitoral, veiculada no domingo, 12, pedindo votos e declarando seus apoios.</p><p>Ao Di&aacute;rio, Celi garantiu que pretende usar a imagem do ex-presidente, bem como de Haddad novamente e que recebeu muitas mensagens nas redes sociais com a apari&ccedil;&atilde;o dos petistas. "Tenho muito orgulho de ter o Lula e o Haddad na nossa campanha. Muito me honra porque o PT tem um legado muito grande dos governos Lula e Dilma em Rio Preto, principalmente &agrave;s popula&ccedil;&otilde;es mais necessitadas", afirmou ela, que citou obras realizadas pelo ex-prefeito Valdomiro Lopes (PSB) obtidas junto aos governos petistas, como moradias populares, tratamento de esgoto, mobilidade urbana e UBSs.</p><p><strong>Em Bebedouro</strong></p><p>Carlos Orpham, candidato a prefeito pelo PT em Bebedouro, tamb&eacute;m se agarrou ao discurso do partido, mantendo a letra e o n&uacute;mero de sua propaganda em vermelho, bem como exibindo a estrela do partido com evid&ecirc;ncia. O prefeitur&aacute;vel, que disputa a cadeira do Executivo local pela segunda vez, surge nas fotos inclusive com m&aacute;scara vermelha, mas disse que ainda n&atilde;o conseguiu uma grava&ccedil;&atilde;o espec&iacute;fica de apoio de Lula ou Haddad, pois "grandes lideran&ccedil;as partid&aacute;rias est&atilde;o priorizando onde tem TV".</p><p>Orpham, entretanto, afirmou j&aacute; ter recebido v&iacute;deos de apoio de outras figuras pol&iacute;ticas, como Edinho, atual prefeito de Araraquara e ex-ministro de Dilma, e M&aacute;rcia Lia, deputada estadual. O candidato tamb&eacute;m lembrou que em sua propaganda de r&aacute;dio &eacute; veiculada uma fala que Lula gravou em geral pedindo para votarem nos candidatos do PT.</p>'
-        title={{
-          fontSize: ['38px', '62px'],
-          mb: ['2', '2'],
-          value: 'Adiamento das eleições para 15 e 29 de novembro é aprovado no Senado'
-        }}
-        topimage={{
-          amp: false,
-          image: true,
-          caption: {
-            fontFamily: 'secondary',
-            fontSize: ['14px', '14px'],
-            lineHeight: ['130%', '130%'],
-            show: true,
-            value: 'O advogado detalha que direitos afetados pela instalação do pedágio na Mogi-Dutra e Mogi-Bertioga / Divulgação/ PMMC',
-          },
-          mb: ['5', '8'],
-          value: 'https://www.odiariodemogi.net.br/image/policy:1.15230.1621460050:1621460050/image.jpg?f=2x1&w=1000'
-        }}
-      />
-    </ThemeProvider>
+    <WithThemeProvider theme={odiariodemogi}>
+      <Block alignx='center' width='100%'>
+        <RenderArticle />
+      </Block>
+    </WithThemeProvider>
   );
 };
 
