7e7ca594499a4d97dc69ea5d4a1e48d576cf8e56
martinezpoa <mauricio@xproglabs.com>
2021-06-22 18:16:44 -0300

    parametriza width no content do Menu

--- components/SideMenu/Menu/index.js
+++ components/SideMenu/Menu/index.js
@@ -5,14 +5,14 @@ import {withTheme} from 'styled-components';
 import Block from '../../Block';
 import {Backdrop, Content} from './styled';
 
-const Menu = ({backgroundColor, children, close, open, menuAnchor, px, py}) => {
+const Menu = ({backgroundColor, children, close, open, menuAnchor, px, py, width}) => {
 
   if (!open) return null;
 
   return (
     <Block>
       <Backdrop onClick={close} />
-      <Content $backgroundColor={backgroundColor} px={px} py={py} menuAnchor={menuAnchor}>
+      <Content $backgroundColor={backgroundColor} px={px} py={py} menuAnchor={menuAnchor} width={width}>
         {children}
       </Content>
     </Block>
@@ -31,7 +31,8 @@ Menu.propTypes = {
   menuAnchor: PropTypes.oneOf(['left', 'right']),
   open: PropTypes.bool,
   px: PropTypes.number,
-  py: PropTypes.number
+  py: PropTypes.number,
+  width: PropTypes.string
 };
 
 export default withTheme(Menu);
