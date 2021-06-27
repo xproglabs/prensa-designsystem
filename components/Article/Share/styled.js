f902ae2632e857acce802c4803171c696c6c259b
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-06-27 14:20:48 -0300

    Remove código não utilizado em Article/Share

--- components/Article/Share/styled.js
+++ components/Article/Share/styled.js
@@ -1,28 +1,4 @@
-// import {get} from 'lodash';
 import styled from 'styled-components';
-
-// import {margin} from '../../../styled-system/margin';
-
-// const getColor = props => {
-//   const theme = get(props, 'theme.colors');
-//   const value = get(theme, props.$color);
-//   return value;
-// };
-
-// const IconContainer = styled.svg`
-//   width: ${props => props.$width}px;
-//   height: ${props => props.$height}px;
-//   fill: ${getColor};
-// `;
-
-// const IconLink = styled.a`
-//   ${margin};
-//   height: max-content;
-//   width: max-content;
-//   &:hover {
-//     opacity: 0.8;
-//   }
-// `;
 
 const Container = styled.div`
   width: 100%;
