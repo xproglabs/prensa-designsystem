119ca8ab3bf215fae57ea40a972c77cf7d2c13ca
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-07-06 15:03:41 -0300

    Remove cor primária do ícone ExpandMore

--- prensa-app/components/AccordionEditorial/assets/ExpandMoreIcon.js
+++ prensa-app/components/AccordionEditorial/assets/ExpandMoreIcon.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const ExpandMoreIcon = props => {
   return (
-    <Icon {...props} color='primary1'>
+    <Icon {...props}>
       <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/>
     </Icon>
   );
