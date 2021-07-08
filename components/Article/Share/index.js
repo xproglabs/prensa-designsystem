b713758227962489687211c7dc1cad01a620020c
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-07-08 00:24:20 -0300

    Adiciona possibilidade de customização de tamanho no Article/Share

--- components/Article/Share/index.js
+++ components/Article/Share/index.js
@@ -3,18 +3,19 @@ import React from 'react';
 
 import * as S from './styled';
 
-const Share = ({facebookPath, fbappid, twitterPath, whatsappPath}) => {
+const Share = ({facebookPath, fbappid, twitterPath, whatsappPath, size}) => {
   return (
     <S.Container facebookPath={fbappid ? facebookPath : null} twitterPath={twitterPath} whatsappPath={whatsappPath}>
-      {fbappid && <amp-social-share type='facebook' width='24' height='24' data-param-app_id={fbappid} />}
-      {twitterPath && <amp-social-share type='twitter' width='24' height='24' />}
-      {whatsappPath && <amp-social-share type='whatsapp' width='24' height='24' />}
+      {fbappid && <amp-social-share type='facebook' width={size} height={size} data-param-app_id={fbappid} />}
+      {twitterPath && <amp-social-share type='twitter' width={size} height={size} />}
+      {whatsappPath && <amp-social-share type='whatsapp' width={size} height={size} />}
     </S.Container>
   );
 };
 
 Share.defaultProps = {
   facebookPath: 'assets/facebook.svg',
+  size: '24',
   twitterPath: 'assets/twitter.svg',
   whatsappPath: 'assets/whatsapp.svg'
 };
@@ -22,8 +23,9 @@ Share.defaultProps = {
 Share.propTypes = {
   fbappid: PropTypes.string,
   facebookPath: PropTypes.string,
+  size: PropTypes.string,
   twitterPath: PropTypes.string,
-  whatsappPath: PropTypes.string,
+  whatsappPath: PropTypes.string
 };
 
 export default Share;
