28e34de039a591928b0767cc5b2a808fc92e8564
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-08-04 12:17:27 -0300

    Corrige imagem no corpo do texto

--- components/Article/TextBody/TextBody.js
+++ components/Article/TextBody/TextBody.js
@@ -52,9 +52,10 @@ const TextBody = (props) => {
   
   const render_image = (value) => {
     if (!value) return null;
-    const image_data = find(images.items, {contentId: value.contentId});
+    const x = value.contentId.split('.');
+    const parsed_policy = `${get(x, '[0]')}.${get(x, '[1]')}`;
+    const image_data = find(images.items, {contentId: parsed_policy});
     if (!image_data) return null;
-    
     return (
       <Block mb={3} maxWidth={bodyWidth} width="100%">
         <TopImage
