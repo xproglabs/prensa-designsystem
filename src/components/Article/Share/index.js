60084f378966e380f717df7fc746c770cabc4f0c
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-08-13 15:51:05 -0300

    Atualiza storybook e remove SocialShareWeb + peerDependency react-share

--- src/components/Article/Share/index.js
+++ src/components/Article/Share/index.js
@@ -1,17 +1,43 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import AmpShare from './AmpShare'
-import Share from './Share'
+import * as S from './styled'
 
-const SocialShare = props => {
-  const { amp } = props
-  if (amp) return <AmpShare {...props} />
-  return <Share {...props} />
+const Share = props => {
+  const { fbappid, size, facebookPath, facebookProps, twitterPath, twitterProps, whatsappPath, whatsappProps, ...otherProps } = props
+  return (
+    <S.Container
+      facebookPath={fbappid ? facebookPath : null}
+      twitterPath={twitterPath}
+      whatsappPath={whatsappPath}
+      $size={size}
+      {...otherProps}
+    >
+      {fbappid && <amp-social-share type='facebook' width={size} height={size} data-param-app_id={fbappid} {...facebookProps} />}
+      {twitterPath && <amp-social-share type='twitter' width={size} height={size} {...twitterProps} />}
+      {whatsappPath && <amp-social-share type='whatsapp' width={size} height={size} {...whatsappProps} />}
+    </S.Container>
+  )
 }
 
-SocialShare.propTypes = {
-  amp: PropTypes.bool
+Share.defaultProps = {
+  align: 'row',
+  facebookPath: 'assets/facebook.svg',
+  size: '24',
+  twitterPath: 'assets/twitter.svg',
+  whatsappPath: 'assets/whatsapp.svg'
 }
 
-export default SocialShare
+Share.propTypes = {
+  fbappid: PropTypes.string,
+  size: PropTypes.string,
+  itemProps: PropTypes.object,
+  facebookPath: PropTypes.string,
+  facebookProps: PropTypes.object,
+  twitterPath: PropTypes.string,
+  twitterProps: PropTypes.object,
+  whatsappPath: PropTypes.string,
+  whatsappProps: PropTypes.object
+}
+
+export default Share
