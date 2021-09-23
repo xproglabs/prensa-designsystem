fc974072c53bbd51c646dc0d951586622d610ee3
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-09-22 22:37:57 -0300

    Adiciona a constante dos templates

---
+++ src/templates/consts.ts
@@ -0,0 +1,13 @@
+export const TEMPLATES_INDEX = {
+  '100': 'template100',
+  '7030': 'template7030',
+  '50': 'template50',
+  '33': 'template30'
+}
+
+export const TEMPLATES_LIST = [
+  TEMPLATES_INDEX[100],
+  TEMPLATES_INDEX[7030],
+  TEMPLATES_INDEX[50],
+  TEMPLATES_INDEX[33]
+]
