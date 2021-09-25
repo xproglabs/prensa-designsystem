ecfd3a8ba651d8b2bf27115ef83fcabc8d2efa40
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-09-24 21:15:14 -0300

    Atualiza os exemplos e os mockups

--- src/examples/Examples.stories.js
+++ src/examples/Examples.stories.js
@@ -3,9 +3,9 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 
 import PageBlock from '../components/PageBlock'
-import DATA_7030 from '../mockup/hojeemdia/7.172.json'
-import DATA_LINE1 from '../mockup/hojeemdia/7.186.json'
-import DATA_LINE2 from '../mockup/hojeemdia/7.204.json'
+import DATA_HOME_100 from '../mockup/hojeemdia/7.172.json'
+import DATA_HOME_7030 from '../mockup/hojeemdia/7.173.json'
+import DATA_HOME_30 from '../mockup/hojeemdia/7.204.json'
 import { theme } from '../styles/demo/hojeemdia'
 import TemplateContainer from '../templates/TemplateContainer'
 
@@ -20,13 +20,30 @@ export default {
   ]
 }
 
-export const HojeEmDiaHome = () => {
+export const Home100 = () => {
   return (
     <TemplateContainer
       mb={[2, 2]}
       mt={[2, 2]}>
-      <PageBlock {...DATA_LINE2} />
-      {/* <PageBlock {...DATA_LINE1} /> */}
+      <PageBlock {...DATA_HOME_100} />
+    </TemplateContainer>
+  )
+}
+export const Home7030 = () => {
+  return (
+    <TemplateContainer
+      mb={[2, 2]}
+      mt={[2, 2]}>
+      <PageBlock {...DATA_HOME_7030} />
+    </TemplateContainer>
+  )
+}
+export const Home30 = () => {
+  return (
+    <TemplateContainer
+      mb={[2, 2]}
+      mt={[2, 2]}>
+      <PageBlock {...DATA_HOME_30} />
     </TemplateContainer>
   )
 }
