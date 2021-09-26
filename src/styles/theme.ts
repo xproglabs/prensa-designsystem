8feaf3c031ea9c7c15b1005dffb0cb34e361978c
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-09-26 20:48:56 -0300

    Adiciona prop templates nos types do theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,19 +2,25 @@ import { LayoutProps } from '../components/Teaser'
 import { merge } from 'lodash'
 import { default_theme } from './default'
 
-export type TeaserTypes = {
-  featured: LayoutProps;
-  featured_related: LayoutProps;
-  image_carousel: LayoutProps;
-  image_featured: LayoutProps;
-  image_four: LayoutProps;
-  image_three: LayoutProps;
-  image_top: LayoutProps;
-  image_two: LayoutProps;
-  image_large_left: LayoutProps;
-  image_small_left: LayoutProps;
-  mostread: LayoutProps;
+export type TemplatesTypes = {
+  default?: Object;
+  featured?: Object;
 }
+
+export type TeaserTypes = {
+  featured?: LayoutProps;
+  featured_related?: LayoutProps;
+  image_carousel?: LayoutProps;
+  image_featured?: LayoutProps;
+  image_four?: LayoutProps;
+  image_three?: LayoutProps;
+  image_top?: LayoutProps;
+  image_two?: LayoutProps;
+  image_large_left?: LayoutProps;
+  image_small_left?: LayoutProps;
+  mostread?: LayoutProps;
+}
+
 export type ThemeTypes = {
   colors?: object;
   fonts?: object;
@@ -22,7 +28,9 @@ export type ThemeTypes = {
   queries?: object;
   radius?: object;
   teasers?: TeaserTypes;
+  templates?: TemplatesTypes;
 }
+
 /**
  * CreateTheme function docs
  * @param {object} data - An object that defines/overrides properties in theme
