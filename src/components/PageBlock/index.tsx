25b3f0fbdd15b057b780dbe886fe481cc239fb70
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-10-06 11:21:31 -0300

    Renomeia constantes no PageBlock

--- src/components/PageBlock/index.tsx
+++ src/components/PageBlock/index.tsx
@@ -28,7 +28,7 @@ const PageBlock = ({
   // console.log(`[PRENSA] PageBlock ${cid} ${name} ${type}`)
   if(type === 'template100') {
     const slot100_block = selectTemplateFromTheme({ block: 'slot100', slot: slot1, templates })
-    const slot100 = selectLayoutFromTemplate({ block: slot100_block, slot: slot1 })
+    const slot100_layout = selectLayoutFromTemplate({ block: slot100_block, slot: slot1 })
     const slot1_spaces = selectLayoutColsFromSlot(slot1.len1, slot100_block.spaces)
     return (
       <Template100
@@ -36,7 +36,7 @@ const PageBlock = ({
           column_items: slot1_spaces + 1,
           column_padding: slot1_spaces,
           domain,
-          layouts: slot100,
+          layouts: slot100_layout,
           slot: slot1.list1
         }}
       />
@@ -45,8 +45,8 @@ const PageBlock = ({
   if(type === 'template7030') {
     const slot70_block = selectTemplateFromTheme({ block: 'slot70', slot: slot1, templates })
     const slot30_block = selectTemplateFromTheme({ block: 'slot30', slot: slot2, templates })
-    const slot70 = selectLayoutFromTemplate({ block: slot70_block, slot: slot1 })
-    const slot30 = selectLayoutFromTemplate({ block: slot30_block, slot: slot2 })
+    const slot70_layout = selectLayoutFromTemplate({ block: slot70_block, slot: slot1 })
+    const slot30_layout = selectLayoutFromTemplate({ block: slot30_block, slot: slot2 })
     const slot1_spaces = selectLayoutColsFromSlot(slot1.len1, slot70_block.spaces)
     const slot2_spaces = selectLayoutColsFromSlot(slot2.len1, slot30_block.spaces)
     return (
@@ -55,14 +55,14 @@ const PageBlock = ({
           column_items: slot1_spaces + 1,
           column_padding: slot1_spaces,
           domain,
-          layouts: slot70,
+          layouts: slot70_layout,
           slot: slot1.list1
         }}
         slot30={{
           column_items: slot2_spaces + 1,
           column_padding: slot2_spaces,
           domain,
-          layouts: slot30,
+          layouts: slot30_layout,
           slot: slot2.list1
         }}
       />
@@ -72,9 +72,9 @@ const PageBlock = ({
     const slotLeft_block = selectTemplateFromTheme({ block: 'slot30', slot: slot1, templates })
     const slotCenter_block = selectTemplateFromTheme({ block: 'slot30', slot: slot2, templates })
     const slotRight_block = selectTemplateFromTheme({ block: 'slot30', slot: slot3, templates })
-    const slotLeft = selectLayoutFromTemplate({ block: slotLeft_block, slot: slot1 })
-    const slotCenter = selectLayoutFromTemplate({ block: slotCenter_block, slot: slot2 })
-    const slotRight = selectLayoutFromTemplate({ block: slotRight_block, slot: slot3 })
+    const slotLeft_layout = selectLayoutFromTemplate({ block: slotLeft_block, slot: slot1 })
+    const slotCenter_layout = selectLayoutFromTemplate({ block: slotCenter_block, slot: slot2 })
+    const slotRight_layout = selectLayoutFromTemplate({ block: slotRight_block, slot: slot3 })
     const slot1_spaces = selectLayoutColsFromSlot(slot1.len1, slotLeft_block.spaces)
     const slot2_spaces = selectLayoutColsFromSlot(slot2.len1, slotCenter_block.spaces)
     const slot3_spaces = selectLayoutColsFromSlot(slot3.len1, slotRight_block.spaces)
@@ -84,36 +84,36 @@ const PageBlock = ({
           column_items: slot1_spaces + 1,
           column_padding: slot1_spaces,
           domain,
-          layouts: slotLeft,
+          layouts: slotLeft_layout,
           slot: slot1.list1
         }}
         slotCenter={{
           column_items: slot2_spaces + 1,
           column_padding: slot2_spaces,
           domain,
-          layouts: slotCenter,
+          layouts: slotCenter_layout,
           slot: slot2.list1
         }}
         slotRight={{
           column_items: slot3_spaces + 1,
           column_padding: slot3_spaces,
           domain,
-          layouts: slotRight,
+          layouts: slotRight_layout,
           slot: slot3.list1
         }}
       />
     )
   }
-  const slot100_block = selectTemplateFromTheme({ block: 'slot100', slot: slot1, templates })
-  const slot100 = selectLayoutFromTemplate({ block: slot100_block, slot: slot1 })
-  const slot1_spaces = selectLayoutColsFromSlot(slot1.len1, slot100_block.spaces)
+  const slowWrap_block = selectTemplateFromTheme({ block: 'slot100', slot: slot1, templates })
+  const slowWrap_layout = selectLayoutFromTemplate({ block: slowWrap_block, slot: slot1 })
+  const slot1_spaces = selectLayoutColsFromSlot(slot1.len1, slowWrap_block.spaces)
   return (
     <TemplateWrap
       slotItems={{
         column_items: slot1_spaces + 1,
         column_padding: slot1_spaces,
         domain,
-        layouts: slot100,
+        layouts: slowWrap_layout,
         slot: slot1.list1
       }}
     />
