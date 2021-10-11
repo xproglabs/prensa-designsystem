dda952b84f3c43a780b899587fc4deccd733d7ed
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-10-11 00:08:21 -0300

    Atualiza os estilos de demonstração do hed

--- src/styles/demo/hed/index.stories.tsx
+++ src/styles/demo/hed/index.stories.tsx
@@ -1,12 +1,12 @@
-import PageBlock from 'components/PageBlock'
-import TemplateContainer from 'components/Templates/TemplateContainer'
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 
+import PageBlock from '../../../components/PageBlock'
+import TemplateContainer from '../../../components/Templates/TemplateContainer'
 import DataHomeLinha1 from './data/7.153.json'
 import DataHomeLinha2 from './data/7.155.json'
 import DataHomeLinha3 from './data/7.161.json'
-// import DATA_HOME_100 from './data/7.172.json'
+import DataHomeTimes from './data/7.181.json'
 import DATA_HOME_7030 from './data/7.173.json'
 import DATA_HOME_FEAT from './data/7.174.json'
 import DATA_HOME_30 from './data/7.204.json'
@@ -25,6 +25,7 @@ export default {
 export const HomeLinha1 = () => {
   return (
     <TemplateContainer
+      background='neutral10'
       mb={[2, 2]}
       mt={[2, 2]}>
       <PageBlock {...DataHomeLinha1} />
@@ -34,6 +35,7 @@ export const HomeLinha1 = () => {
 export const HomeLinha2 = () => {
   return (
     <TemplateContainer
+      background='neutral10'
       mb={[2, 2]}
       mt={[2, 2]}>
       <PageBlock {...DataHomeLinha2} />
@@ -43,15 +45,27 @@ export const HomeLinha2 = () => {
 export const HomeLinha3 = () => {
   return (
     <TemplateContainer
+      background='neutral10'
       mb={[2, 2]}
       mt={[2, 2]}>
       <PageBlock {...DataHomeLinha3} />
     </TemplateContainer>
   )
 }
+export const HomeTimes = () => {
+  return (
+    <TemplateContainer
+      background='neutral10'
+      mb={[2, 2]}
+      mt={[2, 2]}>
+      <PageBlock {...DataHomeTimes} />
+    </TemplateContainer>
+  )
+}
 export const Home7030 = () => {
   return (
     <TemplateContainer
+      background='neutral10'
       mb={[2, 2]}
       mt={[2, 2]}>
       <PageBlock {...DATA_HOME_7030} />
@@ -61,6 +75,7 @@ export const Home7030 = () => {
 export const Home30 = () => {
   return (
     <TemplateContainer
+      background='neutral10'
       mb={[2, 2]}
       mt={[2, 2]}>
       <PageBlock {...DATA_HOME_30} />
@@ -70,6 +85,7 @@ export const Home30 = () => {
 export const HomeFeatured = () => {
   return (
     <TemplateContainer
+      background='neutral10'
       mb={[2, 2]}
       mt={[2, 2]}>
       <PageBlock {...DATA_HOME_FEAT} />
@@ -80,11 +96,13 @@ export const HomeFeatured = () => {
 export const HomeFull = () => {
   return (
     <TemplateContainer
+      background='neutral10'
       mb={[2, 2]}
       mt={[2, 2]}>
       <PageBlock {...DataHomeLinha1} />
       <PageBlock {...DataHomeLinha2} />
       <PageBlock {...DataHomeLinha3} />
+      <PageBlock {...DataHomeTimes} />
       <PageBlock {...DATA_HOME_7030} />
       <PageBlock {...DATA_HOME_30} />
       <PageBlock {...DATA_HOME_FEAT} />
