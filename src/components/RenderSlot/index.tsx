8c46e4a6fad6db6e8d71f2a5a2d076f9a48bba32
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-10-11 13:57:06 -0300

    Move a função renderSpaceSlot para o util

--- src/components/RenderSlot/index.tsx
+++ src/components/RenderSlot/index.tsx
@@ -1,11 +1,14 @@
 import { map } from 'lodash'
-import React, { cloneElement, ReactElement } from 'react'
+import React from 'react'
 import { withTheme } from 'styled-components'
 
 import Block from '../Block'
 import Teaser from '../Teaser'
 import { RenderSlotProps } from './types'
-import { parseTeaserProps } from './utils'
+import { 
+  parseTeaserProps,
+  renderSpaceSlot
+} from './utils'
 
 const RenderSlot = ({
   color,
@@ -22,18 +25,9 @@ const RenderSlot = ({
 }: RenderSlotProps) => {
   const { teasers } = theme
   const column_width = `calc((100% - (${column_padding} * 24px)) / ${column_items})`
-  /**
-   * Render_space function
-   * @param component Expects a ReactElement
-   * @returns a React cloneElement hook for rendering the component passed as a prop
-   */
-  const render_space = (component: ReactElement) => {
-    if (!component) return null
-    return cloneElement(component)
-  }
   return (
     <React.Fragment>
-      {render_space(spaceA)}
+      {renderSpaceSlot(spaceA)}
       {map(slot, (item, key: number) => {
         let teaser_props = parseTeaserProps(key, layout, layouts, slot, teasers)
         if(!teaser_props)
@@ -54,7 +48,7 @@ const RenderSlot = ({
           </Block>
         )
       })}
-      {render_space(spaceB)}
+      {renderSpaceSlot(spaceB)}
     </React.Fragment>
   )
 }
