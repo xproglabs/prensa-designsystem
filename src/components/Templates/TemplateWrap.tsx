43ac10e767c75674d5a2c834d73adbe6b9d9494c
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-10-17 02:37:50 -0300

    Atualiza a diagramação do template100

--- src/components/Templates/TemplateWrap.tsx
+++ src/components/Templates/TemplateWrap.tsx
@@ -24,6 +24,7 @@ const TemplateWrap = ({ slotItems }: TemplateWrapProps) => {
       width="100%">
       {(renderSpaceSlot(slotItems.spaceA))}
       <RenderSlot {...slotItems} />
+      {(renderSpaceSlot(slotItems.spaceB))}
     </Block>
   )
 }
