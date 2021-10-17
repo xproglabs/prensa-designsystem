578ec13154f79e2aa1fc3ee831b4151e37ef9c66
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-10-17 20:25:17 -0300

    Condiciona o Render Circle a prop enabled

--- src/components/Teaser/RenderCircle.tsx
+++ src/components/Teaser/RenderCircle.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 import * as S from './styled'
 
 const RenderCircle = ({ most_read_circle }) => {
+  const is_enabled = most_read_circle && most_read_circle.enabled
+  if (!is_enabled)
+    return <></>
   return (
     <S.Circle
       most_read_circle={most_read_circle}>
