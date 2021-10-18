9dde7bdfd0d4f8e7d98296b152002c64cb762d9c
gbombassaro <giovanni@xproglabs.com>
2021-10-17 21:28:25 -0300

    Reforma componente Icon para ts

---
+++ src/components/Icon/types.ts
@@ -0,0 +1,11 @@
+import { IconTokens } from '@prensa_tokens'
+
+export interface IconProps {
+  children?: any;
+  color?: string;
+  height?: string;
+  viewBox?: string;
+  width?: string;
+  path?: IconTokens;
+  theme?: any;
+}
