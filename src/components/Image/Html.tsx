5c320a62741f8ff89f00bc9e7117b2a94bf83ff9
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-10-17 22:34:18 -0300

    Permite alternar imagem amp e imagem html

---
+++ src/components/Image/Html.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import * as S from './styled'
+import * as T from './types'
+
+const RenderHtmlImage = (props) => {
+  if (props.renderType === 'responsive') {
+    return (
+      <S.HtmlBackgroundStyled
+        className={props.className}
+        href={props.path}
+      />
+    )
+  }
+  return (
+    <S.HtmlImageStyled
+      className={props.className}
+      src={props.path}
+    />
+  )
+}
+
+const HtmlImage = ({
+  custom_class,
+  layout_desktop,
+  layout_mobile,
+  title
+}: T.ImageHtmlProps) => {
+  return (
+    <>
+      {layout_desktop.enabled && (
+        <RenderHtmlImage
+          className='hide_mobile'
+          renderType={layout_desktop.type}
+          path={layout_desktop.path}
+        />
+      )}
+      {layout_mobile.enabled && (
+        <RenderHtmlImage
+          className='hide_desktop'
+          renderType={layout_mobile.type}
+          path={layout_desktop.path}
+        />
+      )}
+    </>
+  )
+}
+
+export default HtmlImage
