eb927906498280dad2dc5d5ae4b63ae2f32f73ab
gbombassaro <giovanni@xproglabs.com>
2021-10-17 21:32:40 -0300

    Novo diretório em /styles para armazenar a config do SectionTitle do projeto

---
+++ src/styles/demo/hed/section_title/index.ts
@@ -0,0 +1,19 @@
+import { SectionTitleLayout } from '../../../../components/SectionTitle/types'
+
+export const SECTION_TITLE: SectionTitleLayout = {
+  area: {
+    mb: [2, 3],
+    pl: [1, 1],
+    pt: [1, 1],
+    bt: '1px',
+    bl: '1px',
+    borderColor: 'primary1'
+  },
+  text: {
+    font_size: ['24px', '24px'],
+    line_height: ['28px', '28px'],
+  },
+  icon: {
+    path: 'icon_info'
+  }
+}
