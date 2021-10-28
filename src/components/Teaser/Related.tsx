0a558d33bf70009c96dc447f99879f7382770fb8
gbombassaro <giovanni@xproglabs.com>
2021-10-27 21:48:37 -0300

    Implementa nova lógica para color no RelatedItem

--- src/components/Teaser/Related.tsx
+++ src/components/Teaser/Related.tsx
@@ -9,25 +9,25 @@ const Related = ({
   items,
   layout
 }: RelatedProps) => {
+  const items_color = layout.color || color
   return (
     <RelatedArea>
-      {(map(items, (item, key) => {
+      {map(items, (item, key) => {
         const item_path = get(item, 'url', false) || get(item, 'path', '')
         return (
           <RelatedItem
             key={key}
-            color={layout.color}
+            color={items_color}
             font_size={layout.font_size}
             font_weight={layout.font_weight}
             icon={layout?.icon}
-            icon_color={color}
             line_height={layout.line_height}
             item_path={item_path}
             mb={layout.mb}>
             {item.name}
           </RelatedItem>
         )
-      }))}
+      })}
     </RelatedArea>
   )
 }
