d6d9b9cc2af07c40f2c2de036898181979df1e0b
gbombassaro <giovanni@xproglabs.com>
2021-10-27 21:49:46 -0300

    Monta exemplo com o teaser related

--- src/styles/demo/hed/index.stories.tsx
+++ src/styles/demo/hed/index.stories.tsx
@@ -20,6 +20,7 @@ import DATA_HOME_30 from './data/7.204.json'
 import DataHomeFull from './data/home.json'
 import { preview_editable } from './editable'
 import { theme } from './index'
+import Data7795 from './data/7.795.json'
 
 export default {
   title: 'Templates/HojeEmDia',
@@ -164,6 +165,22 @@ export const HomeFeatured = () => {
       mt={[2, 2]}>
       <PageBlock
         {...DATA_HOME_FEAT}
+        amp={false}
+        domain={domain}
+        preview={preview_editable}
+        slot_parser={slot_parser}
+      />
+    </TemplateContainer>
+  )
+}
+export const HomeFeaturedNoImageWithRelated = () => {
+  return (
+    <TemplateContainer
+      background='neutral10'
+      mb={[2, 2]}
+      mt={[2, 2]}>
+      <PageBlock
+        {...Data7795}
         amp={false}
         domain={domain}
         preview={preview_editable}
