dfe1ec291205aa7a9014390d01d69c3af6e79629
gbombassaro <giovanni@xproglabs.com>
2021-11-03 15:13:38 -0300

    Atualiza configuração dos teasers para o template latest_news

--- src/styles/demo/hed/templates/latest_news.ts
+++ src/styles/demo/hed/templates/latest_news.ts
@@ -1,17 +1,6 @@
-export const TEMPLATE_LATEST_NEWS = {
-  slot100: {
-    related: {
-      enabled: false
-    },
-    layout: {
-      0: ['teaser_latest'],
-      1: ['teaser_latest'],
-      2: ['teaser_latest', 'teaser_latest'],
-      3: ['teaser_latest', 'teaser_latest', 'teaser_latest'],
-      4: ['teaser_latest', 'teaser_latest', 'teaser_latest', 'teaser_latest'],
-    },
-    spaces: [0, 1, 2, 3],
-  },
+import { CreateTemplate } from 'prensa'
+
+export const TEMPLATE_LATEST_NEWS = CreateTemplate({
   slot70: {
     related: {
       enabled: false
@@ -19,19 +8,15 @@ export const TEMPLATE_LATEST_NEWS = {
     layout: {
       0: ['teaser_latest']
     },
-    spaces: false,
+    spaces: [0, 0, 0, 0]
   },
   slot30: {
     related: {
       enabled: false
     },
     layout: {
-      0: ['teaser_image_top_large'],
-      1: ['teaser_image_small'],
-      2: ['teaser_image_small', 'teaser_image_small'],
-      3: ['teaser_image_small', 'teaser_image_small', 'teaser_image_small'],
-      4: ['teaser_image_small', 'teaser_image_small', 'teaser_image_small', 'teaser_image_small'],
+      0: ['teaser_image_small']
     },
     spaces: [0, 0, 0, 0],
   }
-}
+})
