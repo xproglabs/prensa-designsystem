29a157bef5e10f1aefd4031253ade78f51041340
gbombassaro <giovanni@xproglabs.com>
2021-11-07 19:07:56 -0300

    Adiciona exemplo com newsTitle ativo

--- src/components/AuthorProfile/AuthorProfile.stories.tsx
+++ src/components/AuthorProfile/AuthorProfile.stories.tsx
@@ -12,7 +12,8 @@ export default {
 export const Default = () => {
   return (
     <ThemeProvider theme={theme}>
-      <AuthorProfile 
+      <AuthorProfile
+        containerProps={{ mb: 1 }}
         authorTitle='Aldecir Xavier'
         columnTitle='Preto no Branco'
         image={{
@@ -20,6 +21,16 @@ export const Default = () => {
           mobile_path: 'https://qa.hojeemdia.com.br/image/policy:1.787226.1628829880:1628829880/image.jpg?f=1x1&w=600',
           title: 'Imagem de perfil do colunista'
         }}
+      />
+      <AuthorProfile
+        authorTitle='Aldecir Xavier'
+        columnTitle='Preto no Branco'
+        image={{
+          desktop_path: 'https://qa.hojeemdia.com.br/image/policy:1.787226.1628829880:1628829880/image.jpg?f=1x1&w=600',
+          mobile_path: 'https://qa.hojeemdia.com.br/image/policy:1.787226.1628829880:1628829880/image.jpg?f=1x1&w=600',
+          title: 'Imagem de perfil do colunista'
+        }}
+        newsTitle='Conspiração'
       />
     </ThemeProvider>
   )
