782ee05103a442c21a180c8051e755a5813f7759
gbombassaro <giovanni@xproglabs.com>
2021-11-14 23:14:46 -0300

    Melhora documentação em CreateTemplate

--- styles/templates/types.ts
+++ styles/templates/types.ts
@@ -17,15 +17,42 @@ type TemplateLayout = {
 type TemplateSpaces = [number, number, number, number]
 
 type TemplateSlotTypes = {
+  /**
+   * Prensa | CreateTemplate related
+   */
   related: TemplateRelated;
+  /**
+   * Prensa | CreateTemplate layout
+   */
   layout: TemplateLayout;
+  /**
+   * Prensa | CreateTemplate spaces
+   * @param slotLength Expects slot items length
+   * @param spaces Expects the amount of columns that the content will be splitted (list key is items amount)
+   * @returns Configuration for matching items amount | 0
+   */
   spaces: TemplateSpaces;
 }
 
 export type CreateTemplateTypes = {
+  /**
+   * Prensa | CreateTemplate slot100
+   */
   slot100?: TemplateSlotTypes;
+  /**
+   * Prensa | CreateTemplate slot70
+   */
   slot70?: TemplateSlotTypes;
+  /**
+   * Prensa | CreateTemplate slot50
+   */
   slot50?: TemplateSlotTypes;
+  /**
+   * Prensa | CreateTemplate slot30
+   */
   slot30?: TemplateSlotTypes;
+  /**
+   * Prensa | CreateTemplate slotWrap
+   */
   slotWrap?: TemplateSlotTypes;
 }
