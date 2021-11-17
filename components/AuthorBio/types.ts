626a92b8901d76140a1948e4130e8b3d41a45bfa
gbombassaro <giovanni@xproglabs.com>
2021-11-17 00:20:41 -0300

    Refatora types para as interfaces AuthorBioProps, AuthorTitleProps e ContainerProps
    Essas interfaces definem o modelo de sugestão de props para as props de estilização como containerProps, authorBioProps etc

--- components/AuthorBio/types.ts
+++ components/AuthorBio/types.ts
@@ -1,43 +1,71 @@
+import { RadiusTokens, ColorTokens, FontTokens } from '@prensa_tokens'
+
+export type SpacingType = number | string
 export interface AuthorBioProps {
   children?: any;
-  color?: string;
+  color?: ColorTokens;
+  element: string;
+  fontFamily?: FontTokens;
   fontSize?: string;
   fontWeight?: number;
+  mb?: SpacingType;
 }
 
 export interface AuthorTitleProps {
   children?: any;
-  color?: string;
+  color?: ColorTokens;
+  element: string;
+  fontFamily?: FontTokens;
   fontSize?: string;
   fontWeight?: number;
+  mb?: SpacingType;
 }
 
 export interface ContainerProps {
-  authorBio?: string;
-  authorBioProps?: AuthorBioProps
-  authorTitle?: string;
-  authorTitleProps?: AuthorTitleProps;
-  b?: string;
-  bgColor?: string;
-  borderColor?: string;
   children?: any;
-  color?: string;
-  columnTitle?: string;
-  columnTitleProps?: ColumnTitleProps;
-  containerProps?: ContainerProps;
-  fontSize?: string;
-  fontWeight?: number;
-  hasFacebook?: boolean;
-  hasInstagram?: boolean;
-  hasLinkedin?: boolean;
-  hasTwitter?: boolean;
-  mb?: string | number;
-  radius?: string;
+  mx: SpacingType;
+  my: SpacingType;
+  pt?: SpacingType;
+  pr?: SpacingType;
+  pb?: SpacingType;
+  pl?: SpacingType;
+  mt?: SpacingType;
+  mr?: SpacingType;
+  mb?: SpacingType;
+  ml?: SpacingType;
+  bt?: string;
+  br?: string;
+  bb?: string;
+  bl?: string;
+  b?: string;
+  borderColor?: ColorTokens;
+  borderStyle?: string;
+  radius?: RadiusTokens;
+  width?: string;
 }
 
-export interface ContainerCenterProps {
-  children?: any;
-}
+// export interface ContainerProps {
+//   authorBio?: string;
+//   authorBioProps?: AuthorBioProps
+//   authorTitle?: string;
+//   authorTitleProps?: AuthorTitleProps;
+//   b?: string;
+//   bgColor?: string;
+//   borderColor?: string;
+//   children?: any;
+//   color?: string;
+//   columnTitle?: string;
+//   columnTitleProps?: ColumnTitleProps;
+//   containerProps?: ContainerProps;
+//   fontSize?: string;
+//   fontWeight?: number;
+//   hasFacebook?: boolean;
+//   hasInstagram?: boolean;
+//   hasLinkedin?: boolean;
+//   hasTwitter?: boolean;
+//   mb?: string | number;
+//   radius?: string;
+// }
 
 export interface ContainerImageProps {
   children?: any;
