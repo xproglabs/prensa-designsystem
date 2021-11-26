e2f5f8928d7bd1675e8f512bba3fb0638d61250a
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-11-26 12:32:08 -0300

    Adiciona o text-decoration como parametro no link

--- components/Link/types.ts
+++ components/Link/types.ts
@@ -2,6 +2,7 @@ import React from 'react'
 
 export interface StyledLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   hoverOpacity?: number;
+  textDecoration?: string;
 }
 export interface LinkProps extends StyledLinkProps {
   children: any;
