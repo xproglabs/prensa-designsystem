1301971e5a91805bdbdd618083996012003cb426
Fernando Bombassaro <fernando.bombassaro@gmail.com>
2021-11-27 00:56:47 -0300

    Adiciona o pageblock position 3 do omogi

--- styles/demo/omogi/templates/template_default.ts
+++ styles/demo/omogi/templates/template_default.ts
@@ -15,5 +15,31 @@ export const TEMPLATE_DEFAULT = CreateTemplate({
     },
     spaces: [0, 0, 0, 0],
     space_bottom: [1, 1],
+  },
+  slot70: {
+    layout: {
+      0: ['default'],
+      1: ['default'],
+      2: ['big', 'default'],
+      3: ['default', 'default', 'default'],
+      4: ['default', 'default', 'default', 'default'],
+    },
+    min_height: ['auto', '350px'],
+    related: {
+      enabled: false
+    },
+    spaces: [0, 0, 0, 0],
+    space_bottom: [1, 1],
+  },
+  slot100: {
+    layout: {
+      0: ['default']
+    },
+    min_height: ['auto', '350px'],
+    related: {
+      enabled: false
+    },
+    spaces: [2],
+    space_bottom: [1, 1],
   }
 })
