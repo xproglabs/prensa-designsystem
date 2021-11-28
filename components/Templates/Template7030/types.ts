71578c87789ec0f5b43856866c48817eedf7f454
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-11-28 20:33:01 -0300

    Move Template7030 para um diretório individual

---
+++ components/Templates/Template7030/types.ts
@@ -0,0 +1,7 @@
+import { RenderSlotProps } from '../../RenderSlot/types'
+
+export interface Template7030Props {
+  slotAds: RenderSlotProps;
+  slot70: RenderSlotProps;
+  slot30: RenderSlotProps;
+}
