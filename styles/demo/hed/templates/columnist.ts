294092284ffcd213e9ba13edd7dea8b06169e09f
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-11-28 20:33:45 -0300

    Configura exemplos do TemplateColumnist com slot100, slot7030 e slot30

--- styles/demo/hed/templates/columnist.ts
+++ styles/demo/hed/templates/columnist.ts
@@ -3,24 +3,23 @@ import { CreateTemplate } from 'prensa'
 export const TEMPLATE_COLUMNIST = CreateTemplate({
   slot100: {
     layout: {
-      0: ['teaser_image_circle'],
-      1: ['teaser_image_circle'],
-      2: ['teaser_image_circle', 'teaser_image_circle'],
-      3: ['teaser_image_circle', 'teaser_image_circle', 'teaser_image_circle'],
-      4: ['teaser_image_circle', 'teaser_image_circle', 'teaser_image_circle', 'teaser_image_circle']
+      0: ['teaser_image_circle']
     },
     min_height: ['auto', '350px'],
-    spaces: [2, 2, 2, 2]
+    spaces: [3]
   },
-  slotWrap: {
+  slot70: {
     layout: {
-      0: ['teaser_image_circle'],
-      1: ['teaser_image_circle'],
-      2: ['teaser_image_circle', 'teaser_image_circle'],
-      3: ['teaser_image_circle', 'teaser_image_circle', 'teaser_image_circle'],
-      4: ['teaser_image_circle', 'teaser_image_circle', 'teaser_image_circle', 'teaser_image_circle']
+      0: ['teaser_image_circle']
     },
     min_height: ['auto', '350px'],
-    spaces: [3, 3, 3, 3]
+    spaces: [1]
+  },
+  slot30: {
+    layout: {
+      0: ['teaser_image_circle']
+    },
+    min_height: ['auto', '350px'],
+    spaces: [0]
   }
 })
