2d4b2ede30f1a75d0191824f1de9e02db260622a
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-11-28 21:25:23 -0300

    Otimiza organização no Template100 usando arquivo styled

--- components/Templates/Template100/index.tsx
+++ components/Templates/Template100/index.tsx
@@ -1,58 +1,11 @@
 import { Block } from 'prensa'
 import React from 'react'
 
-import {
-  isBackgroundTransparent,
-  selectBgColorFromSlot,
-  selectMinHeightFromSlot
-} from '../../PageBlock/utils'
+import { isBackgroundTransparent, selectBgColorFromSlot } from '../../PageBlock/utils'
 import RenderSlot from '../../RenderSlot'
 import { renderSpaceSlot } from '../../RenderSlot/utils'
-import { ColorizedColumnProps, ColumnProps, Template100Props } from './types'
-
-
-const Column = ({ children }: ColumnProps) => (
-  <Block
-    align="column"
-    alignx="left"
-    aligny="top"
-    mb={2}
-    width="100%"
-    lg={{ mb: 0 }}
-  >
-    {children}
-  </Block>
-)
-
-const ColorizedColumn = ({ bgColor, children, minHeight, transparent }: ColorizedColumnProps) => {
-  const slot_customHeight_mobile = `${selectMinHeightFromSlot(minHeight?.[0])}`
-  const slot_customHeight_desktop = `${selectMinHeightFromSlot(minHeight?.[1])}`
-  return (
-    <Block
-      align="column"
-      alignx="left"
-      aligny="top"
-      bgColor={bgColor}
-      custom={slot_customHeight_mobile}
-      mb={transparent ? '0px' : 2}
-      pt={transparent ? '0px' : 2}
-      px={2}
-      width='calc(100% - 32px)'
-      lg={{
-        align: 'row',
-        alignx: 'between',
-        aligny: 'top',
-        custom: `flex-wrap: wrap; ${slot_customHeight_desktop}`,
-        mb: transparent ? '0px' : 3,
-        pt: transparent ? '0px' : 3,
-        px: transparent ? '0px' : 3,
-        width: transparent ? '100%' : 'calc(100% - 48px)'
-      }}
-    >
-      {children}
-    </Block>
-  )
-}
+import { ColorizedColumn, Column } from './styled'
+import { Template100Props } from './types'
 
 const Template100 = ({ slotAds, slot100 }: Template100Props) => {
   const slot100_bgColor = selectBgColorFromSlot(slot100)
