3a1d992e3e884764e6141ee6570203048d16ff19
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-12-05 18:01:20 -0300

    Adiciona estilo para centralização no Container

--- components/Article/Embeds/Twitter/types.ts
+++ components/Article/Embeds/Twitter/types.ts
@@ -1,10 +1,10 @@
 export type TwitterEmbedProps = {
   amp: boolean;
   height?: Array<string>;
-  url: string;
   mt?: string | number;
   mr?: string | number;
   mb?: string | number;
   ml?: string | number;
+  url?: string;
   width?: Array<string>;
 }
