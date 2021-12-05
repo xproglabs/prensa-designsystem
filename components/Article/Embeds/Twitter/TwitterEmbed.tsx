46493981453d794431b9ab99983f096c2c82834b
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-12-05 12:49:25 -0300

    Corrige exports do TwitterEmbed

--- components/Article/Embeds/Twitter/TwitterEmbed.tsx
+++ components/Article/Embeds/Twitter/TwitterEmbed.tsx
@@ -4,22 +4,35 @@ import { Tweet } from 'react-twitter-widgets'
 import { Container } from './styles'
 import { TwitterEmbedProps } from './types'
 
-const TwitterEmbed = ({ amp, url, height, mb, ml, mr, mt, width }: TwitterEmbedProps) => {
+const TwitterEmbed = ({
+  amp,
+  url,
+  height,
+  mb,
+  ml,
+  mr,
+  mt,
+  width
+}: TwitterEmbedProps) => {
+
   if (!url) {
     return null
   }
 
   const Web = () => (
     <Tweet
-      options={{
-        className:'Prensa-Twitter-web'
-      }} 
+      options={{ className: 'Prensa-Twitter-web' }}
       tweetId={url}
     />
   )
 
   const Amp = () => (
-    <amp-twitter data-tweetid={url} layout='responsive' height='472' width='552' />
+    <amp-twitter
+      data-tweetid={url}
+      layout='responsive'
+      height='472'
+      width='552'
+    />
   )
 
   return (
@@ -42,4 +55,4 @@ TwitterEmbed.defaultProps = {
   width: ['100%', '720px']
 }
 
-export default TwitterEmbed
+export { TwitterEmbed }
