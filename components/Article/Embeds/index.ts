e1df0d2d85dfd8b20993c507d5cec253b0a69999
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-12-05 19:14:35 -0300

    Implementa FacebookEmbed híbrido

--- components/Article/Embeds/index.ts
+++ components/Article/Embeds/index.ts
@@ -1,4 +1,4 @@
-export { default as Facebook } from './Facebook'
 export { default as Twitter } from './Twitter'
 export { default as Instagram } from './Instagram'
+export * from './Facebook'
 export * from './YouTube'
