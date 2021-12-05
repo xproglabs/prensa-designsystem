4f9909c1a5819ff1e793a145e8f02527e1d3828e
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-12-05 00:00:51 -0300

    Remove px na função getMinWidthValue

--- components/Article/Share/styled.tsx
+++ components/Article/Share/styled.tsx
@@ -2,7 +2,7 @@ import { margin } from 'prensa/styled-system'
 
 export const getMinWidthValue = ({ $size }: any) => {
   if (!$size) return ''
-  return `min-width: ${$size}px`
+  return `min-width: ${$size}`
 }
 
 export const getItemMarginStyle = ({ theme, itemProps }: any) => {
