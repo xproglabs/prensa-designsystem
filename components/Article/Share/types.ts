9a8dc797081375cd958bf509fe709f5ad66806c6
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-12-05 01:22:07 -0300

    Cria nova prop containerProps para permitir customizações no Container dos botões

--- components/Article/Share/types.ts
+++ components/Article/Share/types.ts
@@ -2,8 +2,20 @@ import { FacebookProps } from './FacebookShareButton/types'
 import { TwitterProps } from './TwitterShareButton/types'
 import { WhatsAppProps } from './WhatsAppShareButton/types'
 
+export type ContainerProps = {
+  align?: string;
+  alignx?: string;
+  aligny?: string;
+  mt?: number | string;
+  mr?: number | string;
+  mb?: number | string;
+  ml?: number | string;
+  width?: string;
+}
+
 export interface ShareProps {
   amp: boolean;
+  containerProps?: ContainerProps;
   pageUrl: string;
   size?: string;
   facebookPath?: string;
