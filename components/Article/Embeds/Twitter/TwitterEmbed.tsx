27292dd2f73c716346cc5aafc1e1276c07430ea0
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-12-05 18:23:00 -0300

    Desenvolve e implementa função getTweetId

--- components/Article/Embeds/Twitter/TwitterEmbed.tsx
+++ components/Article/Embeds/Twitter/TwitterEmbed.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Tweet } from 'react-twitter-widgets'
 
 import { TwitterEmbedContainer } from './styled'
 import { TwitterEmbedProps } from './types'
+import { getTweetId } from './utils'
 
 const TwitterEmbed = ({
   amp,
@@ -20,9 +21,11 @@ const TwitterEmbed = ({
     return null
   }
 
+  const tweetId = getTweetId(url)
+
   const Web = () => (
     <Tweet
-      tweetId='841418541026877441'
+      tweetId={tweetId}
       options={{
         align: 'center',
         width: '552px',
@@ -33,7 +36,7 @@ const TwitterEmbed = ({
 
   const Amp = () => (
     <amp-twitter
-      data-tweetid={url}
+      data-tweetid={tweetId}
       layout='responsive'
       height='472'
       width='552'
