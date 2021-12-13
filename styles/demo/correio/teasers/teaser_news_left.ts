dbb2837d144e4df180e43af53abbd6e6f5bdcc0a
martinezpoa <mauricio@xproglabs.com>
2021-12-13 12:13:02 -0300

    formata estilo do teaser_news_left

--- styles/demo/correio/teasers/teaser_news_left.ts
+++ styles/demo/correio/teasers/teaser_news_left.ts
@@ -2,26 +2,29 @@ import { CreateTeaser } from 'prensa'
 
 import { parseResponsiveFont } from '../../../parsers/parseFonts'
 import { tokensFonts } from '../tokens/fonts'
-import { SUBJECT_DEFAULT } from '../tokens/subject'
+import { SUBJECT_DEFAULT_SMALL } from '../tokens/subject'
 
 export const TEASER_NEWS_LEFT = CreateTeaser({
   box: {
     align: ['row', 'row'],
     background: 'white',
     mb: [0, 0],
-    pt: [2, 2],
-    pb: [2, 2]
+    pb: [1, 2]
   },
   box_wrap: {
     align: ['column', 'column'],
     alignx: ['left', 'left'],
-    width: ['calc(100% - 32px - 16px - 80px)', 'calc(100% - 32px - 16px - 80px)']
+    mt: [1, 1],
+    ml: [2, 2],
+    width: ['calc(100% - 16px - 120px)', 'calc(100% - 32px - 16px - 80px)']
   },
   date_time: {
     enabled: true,
+    fontSize: '12px'
   },
   title: {
     enabled: true,
+    font_weight: 900,
     mb: ['4px', '4px'],
     ...parseResponsiveFont(
       tokensFonts.NEWS_TITLE_8,
@@ -31,11 +34,9 @@ export const TEASER_NEWS_LEFT = CreateTeaser({
   image: {
     enabled: true,
     dimension: ['1x1', '1x1'],
-    height: ['80px', '80px'],
-    width: ['80px', '80px'],
-    wrap_width: ['80px', '80px'],
-    ml: [2, 2],
-    mr: [2, 2]
+    height: ['120px', '80px'],
+    width: ['120px', '80px'],
+    wrap_width: ['120px', '80px'],
   },
-  subject: SUBJECT_DEFAULT
+  subject: SUBJECT_DEFAULT_SMALL
 })
