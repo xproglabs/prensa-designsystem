e0cb12e4f361d7c7ac0d77c12ba5033ca1a785a1
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-12-16 09:08:31 -0300

    Adiciona WebImageGallery

---
+++ components/ImageGallery/WebImageGallery.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { WebImageGalleryProps } from './types'
+
+const WebImageGallery = ({ items }: WebImageGalleryProps) => {
+  console.log(items)
+  return (
+    <pre>web carousel</pre>
+  )
+}
+
+export { WebImageGallery }
