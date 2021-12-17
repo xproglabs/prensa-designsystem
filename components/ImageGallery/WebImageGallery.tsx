a22c1ebeae8df3d3052ab63c1c0fb99c8db66765
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-12-17 00:23:55 -0300

    Implementa Carousel na galeria web utilizando react-responsive-carousel e ControlButton

--- components/ImageGallery/WebImageGallery.tsx
+++ components/ImageGallery/WebImageGallery.tsx
@@ -1,11 +1,32 @@
+import { map } from 'lodash'
 import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
+import { ControlButton } from './ControlButton'
+import { Image } from './styled'
 import { WebImageGalleryProps } from './types'
 
 const WebImageGallery = ({ items }: WebImageGalleryProps) => {
-  console.log(items)
+
+  const renderArrowPrev = (onClick, hasArrow) => {
+    return hasArrow && <ControlButton onClick={onClick} />
+  }
+
+  const renderArrowNext = (onClick, hasArrow) => {
+    return hasArrow && <ControlButton onClick={onClick} isNext />
+  }
+
   return (
-    <pre>web carousel</pre>
+    <Carousel
+      showIndicators={false}
+      showStatus={false}
+      showThumbs={false}
+      renderArrowPrev={renderArrowPrev}
+      renderArrowNext={renderArrowNext}
+    >
+      {map(items, item => <Image src={item.value} />)}
+    </Carousel>
   )
 }
 
