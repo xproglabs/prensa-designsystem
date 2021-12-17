0903fe947ed9a35f0981e7f3f0e62456af5186de
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-12-17 01:33:22 -0300

    Ajusta identação do CSS em WebCarousel

--- components/ImageGallery/styled.js
+++ components/ImageGallery/styled.js
@@ -77,79 +77,79 @@ const Image = styled.img`
 `
 
 const WebCarousel = styled(Carousel)`
-.carousel-root {
-  outline: none; 
-}
-.carousel {
-  position: relative;
-  width: 100%; 
-}
-.carousel * {
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  box-sizing: border-box;
-}
-.carousel img {
-  width: 100%;
-  display: inline-block;
-  pointer-events: none; 
-}
-.carousel .carousel {
-  position: relative;
-}
-.carousel.carousel-slider {
-  position: relative;
-  margin: 0;
-  overflow: hidden;
-}
-.carousel .slider-wrapper {
-  overflow: hidden;
-  margin: auto;
-  width: 100%;
-  -webkit-transition: height 0.15s ease-in;
-  -moz-transition: height 0.15s ease-in;
-  -ms-transition: height 0.15s ease-in;
-  -o-transition: height 0.15s ease-in;
-  transition: height 0.15s ease-in;
-}
-.carousel .slider-wrapper.axis-horizontal .slider {
-  -ms-box-orient: horizontal;
-  display: -webkit-box;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: -moz-flex;
-  display: -webkit-flex;
-  display: flex;
-}
-.carousel .slider-wrapper.axis-horizontal .slider .slide {
-  flex-direction: column;
-  flex-flow: column;
-}
-.carousel .slider {
-  margin: 0;
-  padding: 0;
-  position: relative;
-  list-style: none;
-  width: 100%;
-}
-.carousel .slider.animated {
-  -webkit-transition: all 0.35s ease-in-out;
-  -moz-transition: all 0.35s ease-in-out;
-  -ms-transition: all 0.35s ease-in-out;
-  -o-transition: all 0.35s ease-in-out;
-  transition: all 0.35s ease-in-out;
-}
-.carousel .slide {
-  min-width: 100%;
-  margin: 0;
-  position: relative;
-  text-align: center;
-}
-.carousel .slide img {
-  width: 100%;
-  vertical-align: top;
-  border: 0;
-}
+  .carousel-root {
+    outline: none; 
+  }
+  .carousel {
+    position: relative;
+    width: 100%; 
+  }
+  .carousel * {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+  .carousel img {
+    width: 100%;
+    display: inline-block;
+    pointer-events: none; 
+  }
+  .carousel .carousel {
+    position: relative;
+  }
+  .carousel.carousel-slider {
+    position: relative;
+    margin: 0;
+    overflow: hidden;
+  }
+  .carousel .slider-wrapper {
+    overflow: hidden;
+    margin: auto;
+    width: 100%;
+    -webkit-transition: height 0.15s ease-in;
+    -moz-transition: height 0.15s ease-in;
+    -ms-transition: height 0.15s ease-in;
+    -o-transition: height 0.15s ease-in;
+    transition: height 0.15s ease-in;
+  }
+  .carousel .slider-wrapper.axis-horizontal .slider {
+    -ms-box-orient: horizontal;
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -moz-flex;
+    display: -webkit-flex;
+    display: flex;
+  }
+  .carousel .slider-wrapper.axis-horizontal .slider .slide {
+    flex-direction: column;
+    flex-flow: column;
+  }
+  .carousel .slider {
+    margin: 0;
+    padding: 0;
+    position: relative;
+    list-style: none;
+    width: 100%;
+  }
+  .carousel .slider.animated {
+    -webkit-transition: all 0.35s ease-in-out;
+    -moz-transition: all 0.35s ease-in-out;
+    -ms-transition: all 0.35s ease-in-out;
+    -o-transition: all 0.35s ease-in-out;
+    transition: all 0.35s ease-in-out;
+  }
+  .carousel .slide {
+    min-width: 100%;
+    margin: 0;
+    position: relative;
+    text-align: center;
+  }
+  .carousel .slide img {
+    width: 100%;
+    vertical-align: top;
+    border: 0;
+  }
 `
 
 export { Container, Image, SubtitleBox, Subtitle, WebCarousel }
