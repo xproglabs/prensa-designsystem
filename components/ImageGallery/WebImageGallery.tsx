74b6121e9801ccc77f97029efc422b84c6b77ca3
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-12-18 11:42:39 -0300

    Implementa caption no WebCarousel

--- components/ImageGallery/WebImageGallery.tsx
+++ components/ImageGallery/WebImageGallery.tsx
@@ -1,6 +1,7 @@
 import { map } from 'lodash'
 import React from 'react'
 
+import { Caption } from './Caption'
 import { ControlButton } from './ControlButton'
 import { WebCarousel } from './styled'
 import { WebImageGalleryProps } from './types'
@@ -24,11 +25,11 @@ const WebImageGallery = ({ items }: WebImageGalleryProps) => {
       renderArrowPrev={renderArrowPrev}
       renderArrowNext={renderArrowNext}
     >
-      {map(items, (item, key) => 
-        <WebImage
-          key={key}
-          src={item.value}
-        />
+      {map(items, (item, key) =>
+        <React.Fragment>
+          <WebImage key={key} src={item.value} />
+          <Caption>{item.caption}</Caption>
+        </React.Fragment>
       )}
     </WebCarousel>
   )
