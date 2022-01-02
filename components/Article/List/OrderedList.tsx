74b193285ff2140677e3e31b4551bb21f6140340
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2022-01-01 22:55:57 -0300

    Limpa espaçamento extra em OrderedList

--- components/Article/List/OrderedList.tsx
+++ components/Article/List/OrderedList.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 
 import { OrderedListProps } from './types'
 
-
 export const OrderedList: ComponentType<OrderedListProps> = styled.ol<OrderedListProps>`
   max-width: calc(${props => props.maxWidth} - 40px);
   width: calc(100% - 40px);
