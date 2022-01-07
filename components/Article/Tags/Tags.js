a28037620bc6b5512b0f5c9525773568056c2743
Pietro <pietro.saraiva@gmail.com>
2022-01-07 09:33:33 -0300

    feat: add transform option in tags

--- components/Article/Tags/Tags.js
+++ components/Article/Tags/Tags.js
@@ -14,6 +14,7 @@ const Tags = ({
   fontWeight,
   items,
   maxWidth,
+  transform,
   radius
 }) => {
   return (
@@ -32,6 +33,7 @@ const Tags = ({
                 fontFamily={fontFamily}
                 fontSize={fontSize[0]}
                 fontWeight={fontWeight}
+                transform={transform}
                 custom='white-space: nowrap;'
               >
                 {item}
@@ -49,7 +51,8 @@ Tags.defaultProps = {
   fontFamily: 'secondary',
   fontSize: ['14px'],
   fontWeight: 700,
-  items: []
+  items: [],
+  transform: 'none'
 }
 
 Tags.propTypes = {
@@ -59,6 +62,7 @@ Tags.propTypes = {
   fontWeight: PropTypes.number,
   items: PropTypes.array,
   maxWidth: PropTypes.string,
+  transform: PropTypes.string,
 }
 
 export default Tags
