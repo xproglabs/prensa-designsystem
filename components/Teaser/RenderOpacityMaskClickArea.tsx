d109600a34ef20a3a1b7660d2a85616e157a67b3
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2022-01-14 12:25:19 -0300

    Cria componente RenderOpacityMaskClickArea no Teaser
    Implementa ImageLink no teaser normal e preview

---
+++ components/Teaser/RenderOpacityMaskClickArea.tsx
@@ -0,0 +1,46 @@
+import { get } from 'lodash'
+import { Link } from 'prensa'
+import React from 'react'
+
+import { ImagePreviewLink } from '../Image/preview'
+
+const RenderOpacityMaskClickArea = ({
+  children,
+  editable,
+  layout,
+  item,
+  item_path,
+  opacity_mask
+}: any) => {
+
+  if (!opacity_mask) {
+    return children
+  }
+
+  const mobile_dim = get(layout, 'image.dimension[0]', '1x1')
+  const desktop_dim = get(layout, 'image.dimension[1]', '1x1')
+  const height = get(layout, 'image.height', 600)
+
+  if (editable?.enabled) {
+    return (
+      <ImagePreviewLink
+        editable={editable}
+        image_props={{ mobile_dim, desktop_dim }}
+        item={item}
+      >
+        {children}
+      </ImagePreviewLink>
+    )
+  }
+
+  return (
+    <Link
+      height={height}
+      href={item_path}
+    >
+      {children}
+    </Link>
+  )
+}
+
+export { RenderOpacityMaskClickArea }
