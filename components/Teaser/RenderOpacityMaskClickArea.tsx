e242623844aea91d84d93e80232f6c3fe311b8f0
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2022-01-16 21:49:01 -0300

    Permite align e height no RenderOpacityMaskClickArea

--- components/Teaser/RenderOpacityMaskClickArea.tsx
+++ components/Teaser/RenderOpacityMaskClickArea.tsx
@@ -5,6 +5,10 @@ import React from 'react'
 import { ImagePreviewLink } from '../Image/preview'
 
 const RenderOpacityMaskClickArea = ({
+  align,
+  alignx,
+  aligny,
+  height,
   children,
   editable,
   layout,
@@ -19,11 +23,14 @@ const RenderOpacityMaskClickArea = ({
 
   const mobile_dim = get(layout, 'image.dimension[0]', '1x1')
   const desktop_dim = get(layout, 'image.dimension[1]', '1x1')
-  const height = get(layout, 'image.height', 600)
 
   if (editable?.enabled) {
     return (
       <ImagePreviewLink
+        align={align}
+        alignx={alignx}
+        aligny={aligny}
+        height={height}
         editable={editable}
         image_props={{ mobile_dim, desktop_dim }}
         item={item}
