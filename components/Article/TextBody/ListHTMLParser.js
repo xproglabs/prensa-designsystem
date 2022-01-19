ca7cc1503a6743c4949b6657ff088fb25319b58a
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2022-01-19 19:43:08 -0300

    feat: Novo arquivo ListHTMLParser e nova função parseListChildren

---
+++ components/Article/TextBody/ListHTMLParser.js
@@ -0,0 +1,53 @@
+import { filter, map } from 'lodash'
+
+import { removeSpaces } from './utils'
+
+export function parseListChildren(child) {
+
+  const list_data = filter(child, { tag: 'li' })
+  const html = []
+  let hasHyperlink = []
+
+  map(list_data, (it) => {
+    map(it.child,
+      (text_item) => {
+        const { attr, child, tag, text, node } = text_item
+
+        if (node === 'element' && tag === 'a') {
+          map(child, hyperlinkedText => {
+            hasHyperlink.push(
+              `<a href="${attr.href}">${hyperlinkedText.text}</a>`
+            )
+          })
+        }
+
+        if (hasHyperlink.length > 0) {
+          if (node === 'text') {
+            hasHyperlink.push(
+              text
+            )
+          }
+        }
+
+        if (hasHyperlink.length === 0) {
+          html.push(
+            removeSpaces(
+              text
+            )
+          )
+        }
+      }
+    )
+
+    if (hasHyperlink.length > 0) {
+      html.push(
+        removeSpaces(
+          hasHyperlink.join('')
+        )
+      )
+      hasHyperlink = []
+    }
+  })
+
+  return html
+}
