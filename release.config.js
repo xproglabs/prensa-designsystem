20b364584d79f750b70b72188bc9003d6460b111
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2022-01-22 10:30:54 -0300

    fix: Novo arquivo de configuração js

---
+++ release.config.js
@@ -0,0 +1,47 @@
+const { parseNotification } = require('./notifications/releaseNotification')
+
+module.exports = {
+  branches: [
+    { name: 'master' },
+    { name: 'qa', prerelease: true }
+  ],
+  plugins: [
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
+    [
+      '@semantic-release/commit-analyzer',
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+    ],
+    [
+      '@semantic-release/npm'
+    ],
+    [
+      '@semantic-release/git', {
+        assets: [
+          '!dist/**/*'
+        ]
+      }
+    ],
+    [
+      'semantic-release-slack-bot',
+      {
+        notifyOnSuccess: true,
+        notifyOnFail: true,
+        onSuccessFunction: data => parseNotification(data),
+        branchesConfig: [
+          {
+            pattern: 'master1',
+            notifyOnSuccess: true,
+            notifyOnFail: true
+          }
+        ]
+      }
+    ] 
+  ]
+}
