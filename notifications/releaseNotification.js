1d6a76329b50efd2aa7ceea68ae47dfa0f5c41aa
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2022-01-22 11:26:52 -0300

    fix: Tentativa de mostrar a versão utilizando variável pré-configurada

--- notifications/releaseNotification.js
+++ notifications/releaseNotification.js
@@ -1,17 +1,14 @@
-function parseNotification(pluginConfig) {
-
-  const message = `*v${pluginConfig.packageVersion}* publicada com sucesso`
-
+function parseNotification() {
   return {
     text: '',
     blocks: [
       {
         type: 'section',
         text: {
-          type: 'text',
-          text: message
+          type: 'plain_text',
+          text: '*v$npm_package_version* publicada com sucesso'
         }
-      }
+      },
     ]
   }
 }
