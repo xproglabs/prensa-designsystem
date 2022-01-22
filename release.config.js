b0895986fbbddb6b34e4f0b4074174f76ceace7b
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2022-01-22 11:53:30 -0300

    fix: Novo teste com mensagem padrão

--- release.config.js
+++ release.config.js
@@ -1,4 +1,4 @@
-const { parseNotification } = require('./notifications/releaseNotification')
+// const { parseNotification } = require('./notifications/releaseNotification')
 const { version } = require('./package.json')
 
 module.exports = {
@@ -35,7 +35,10 @@ module.exports = {
         packageVersion: version,
         notifyOnSuccess: true,
         notifyOnFail: true,
-        onSuccessFunction: data => parseNotification(data),
+        // onSuccessFunction: data => parseNotification(data),
+        onSuccessTemplate: {
+          text: 'A new version of $package_name with version $npm_package_version has been released at $repo_url!'
+        },
         branchesConfig: [
           {
             pattern: 'master1',
