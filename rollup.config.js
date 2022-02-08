ff1eedfdcb5400b0f3d50a0d01ef24a61b75d547
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2022-02-08 00:12:41 -0300

    feat: Cria bundle para o theme

--- rollup.config.js
+++ rollup.config.js
@@ -5,5 +5,7 @@ export default [
   createBundle('icons/index.ts', 'dist/icons'),
   createBundle('styled-system/index.ts', 'dist/styled-system'),
   createBundle('styles/index.ts', 'dist/styles'),
-  createBundle('utils.ts', 'dist/utils')
+  createBundle('utils.ts', 'dist/utils'),
+
+  createBundle('styles/theme/default_theme.ts', 'dist/theme')
 ]
