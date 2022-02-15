174945def46c6ff9047ed8d02256eca3289dfdd1
gbombassaro <giovanni.bombassaro@gmail.com>
2022-02-14 21:44:34 -0300

    feat: Implementa funções border, maxAndMin e radius no Block

--- components/NewBlock/props.ts
+++ components/NewBlock/props.ts
@@ -1,31 +1,40 @@
 import {
   AlignProps,
+  BorderProps,
   ColorProps,
   HeightProps,
   MarginProps,
+  MaxAndMinProps,
   PaddingProps,
+  RadiusProps,
   WidthProps
 } from '@xprog/prensa-css-engine/props'
 import { ReactNode } from 'react'
 
 export interface BlockProps extends
   AlignProps,
+  BorderProps,
   MarginProps,
+  MaxAndMinProps,
   PaddingProps
   {
     children: ReactNode;
     color?: ColorProps['$color'];
     bgColor?: ColorProps['$bgColor'];
     height?: HeightProps['$height'];
+    radius?: RadiusProps['$radius'];
     width: WidthProps['$width'];
   }
 
 export interface StyledBlockProps extends
   AlignProps,
+  BorderProps,
   ColorProps,
   HeightProps,
   MarginProps,
+  MaxAndMinProps,
   PaddingProps,
+  RadiusProps,
   WidthProps
   {
     children: ReactNode;
--- components/NewBlock/styled.tsx
+++ components/NewBlock/styled.tsx
@@ -1,8 +1,11 @@
 import { align } from '@xprog/prensa-css-engine/align'
+import { border } from '@xprog/prensa-css-engine/border'
 import { color } from '@xprog/prensa-css-engine/color'
 import { height } from '@xprog/prensa-css-engine/height'
 import { margin } from '@xprog/prensa-css-engine/margin'
+import { maxAndMin } from '@xprog/prensa-css-engine/maxAndMin'
 import { padding } from '@xprog/prensa-css-engine/padding'
+import { radius } from '@xprog/prensa-css-engine/radius'
 import { width } from '@xprog/prensa-css-engine/width'
 import { ComponentType } from 'react'
 import styled from 'styled-components'
@@ -11,10 +14,13 @@ import { StyledBlockProps } from './props'
 
 const StyledBlock: ComponentType<StyledBlockProps> = styled.div<StyledBlockProps>`
   ${align};
+  ${border};
   ${color};
   ${height};
   ${margin};
+  ${maxAndMin};
   ${padding};
+  ${radius};
   ${width};
 `
 
