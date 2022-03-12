a2f5c5d38a5c772419a358c8a5553716cce72e42
gbombassaro <giovanni.bombassaro@gmail.com>
2022-03-12 02:39:53 -0300

    feat: NewBlock no Article/Share

--- components/Article/Share/types.ts
+++ components/Article/Share/types.ts
@@ -1,21 +1,11 @@
+import { StyledBlockProps } from '../../NewBlock/props'
 import { FacebookProps } from './FacebookShareButton/types'
 import { TwitterProps } from './TwitterShareButton/types'
 import { WhatsAppProps } from './WhatsAppShareButton/types'
 
-export type ContainerProps = {
-  align?: string;
-  alignx?: string;
-  aligny?: string;
-  mt?: number | string;
-  mr?: number | string;
-  mb?: number | string;
-  ml?: number | string;
-  width?: string;
-}
-
 export interface ShareProps {
   amp: boolean;
-  containerProps?: ContainerProps;
+  containerProps?: StyledBlockProps;
   pageUrl: string;
   size?: string;
   facebookPath?: string;
