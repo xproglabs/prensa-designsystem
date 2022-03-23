61054e0060b138029b1cc6eb3eb91877232a1857
Giovanni Bombassaro <giovanni@xproglabs.com>
2022-03-22 22:26:56 -0300

    Update icons/IcLinkedin.tsx

--- icons/IcLinkedin.tsx
+++ icons/IcLinkedin.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const IconLinkedin = (props) => {
   return (
-    <Icon xmlns="http://www.w3.org/2000/svg" {...props}>
+    <Icon xmlns='http://www.w3.org/2000/svg' {...props}>
       <path d="M3.709 9.087v11.346h3.538V9.087H3.709Zm-.154-3.57a1.935 1.935 0 1 0 1.935-1.95 1.942 1.942 0 0 0-1.935 1.95Zm13.391 14.916h3.499v-6.93c0-6.105-6.333-5.59-7.57-2.757V9.087h-3.48v11.346h3.635V14.12c0-2.86 3.916-2.834 3.916 0v6.312Z"/>
     </Icon>
   )
