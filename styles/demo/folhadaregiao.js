fbdd4b108cf3b4f8e195498d3097e5d93892ad0f
maura <maurimartinezpoa@gmail.com>
2022-05-25 18:06:03 -0300

    feat: cria Article exemplo da Folha da Região; insere novas fonts

---
+++ styles/demo/folhadaregiao.js
@@ -0,0 +1,15 @@
+import { theme as default_theme } from '../theme'
+
+export const theme = {
+  ...default_theme,
+  colors: {
+    ...default_theme.colors,
+    primary1: '#0062A7',
+    primary2: '#4B8EBD',
+    primary3: '#0D4166',
+  },
+  fonts: {
+    primary: 'Gelasio',
+    secondary: 'Open Sans'
+  }
+}
