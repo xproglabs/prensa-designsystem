2a1acd1e9bbbe4293bf1569cf40725482c9fb8df
maura <maurimartinezpoa@gmail.com>
2022-05-31 17:35:46 -0300

    fix: ajustes de estilo no teaser news_top_top

--- styles/demo/folhadaregiao/teasers/news_top_top.ts
+++ styles/demo/folhadaregiao/teasers/news_top_top.ts
@@ -5,7 +5,7 @@ import { SUBJECT_DEFAULT } from '../tokens/subjects'
 
 export const TEASER_TOP_TOP = CreateTeaser({
   box: {
-    align: ['row-reverse', 'column'],
+    align: ['column', 'column'],
     background: 'white',
   },
   box_wrap: {
@@ -14,22 +14,22 @@ export const TEASER_TOP_TOP = CreateTeaser({
     ml: [1, 2],
     mr: [1, 2],
     mt: [1, 2],
-    width: ['calc(100% - 96px)', 'calc(100% - 32px)']
+    width: ['calc(100% - 32px)', 'calc(100% - 32px)'],
   },
   title: {
     enabled: true,
     ...parseFonts(
-      tokensFonts.NEWS_TITLE_8,
+      tokensFonts.NEWS_TITLE_6,
       tokensFonts.NEWS_TITLE_6
     )
   },
   image: {
     enabled: true,
-    dimension: ['1x1', '2x1'],
-    height: ['80px', '194px'],
-    layout: ['responsive', 'responsive'],
-    width: ['80px', '100%'],
-    wrap_width: ['80px', '100%'],
+    dimension: ['2x1', '2x1'],
+    height: ['auto', '194px'],
+    layout: ['fixed', 'responsive'],
+    width: ['100%', '100%'],
+    wrap_width: ['100%', '100%'],
   },
   related: {
     enabled: false
