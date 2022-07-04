24f2e1877f59981b568e8c5fb4441d301b4de6a0
gbombassaro <giovanni.bombassaro@gmail.com>
2022-07-04 10:58:22 -0300

    Atualiza theme do storybook

--- storybook/theme.ts
+++ storybook/theme.ts
@@ -1,3 +1,9 @@
 import { CreateTheme } from 'prensa'
 
-export const theme = CreateTheme({})
+export const theme = CreateTheme({
+  factors: {
+    fontSize: 8,
+    lineHeight: 8,
+    letterSpacing: 2
+  }
+})
