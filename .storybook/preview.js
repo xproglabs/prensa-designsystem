ece336f3563945a025579a7f336595ffcf57b4b2
gbombassaro <giovanni.bombassaro@gmail.com>
2022-07-16 13:02:14 -0300

    Atualiza arquivo preview do storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,14 @@
-export const parameters = { layout: 'fullscreen' }
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  layout: 'fullscreen',
+  // options: {
+  //   storySort: {
+  //     order: [
+  //       'Introduction',
+  //       ['Introduction', 'Getting Started'],
+  //       'Edit',
+  //       ['HTML', 'Lazy Loading']
+  //     ]
+  //   }
+  // }
+}
