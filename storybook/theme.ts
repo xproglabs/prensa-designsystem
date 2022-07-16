16305e635cc52157686b59bc671da2d84086ca3f
gbombassaro <giovanni.bombassaro@gmail.com>
2022-07-16 13:02:00 -0300

    Atualiza storybook/theme

--- storybook/theme.ts
+++ storybook/theme.ts
@@ -1,6 +1,10 @@
 import CreateTheme from '../styles/theme'
 
 export const theme = CreateTheme({
+  colors: {
+    primary: '#D13135',
+    secondary: '#298D8C'
+  },
   factors: {
     fontSize: 8,
     lineHeight: 8,
