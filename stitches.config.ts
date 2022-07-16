5386c7c92a7f259e2dd69daa07d8c9658a1c97e1
gbombassaro <giovanni.bombassaro@gmail.com>
2022-07-16 19:37:37 -0300

    Conecta stitches no Prensa

---
+++ stitches.config.ts
@@ -0,0 +1,16 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  prefix: 'prensa',
+  theme: {},
+  utils: {}
+})
