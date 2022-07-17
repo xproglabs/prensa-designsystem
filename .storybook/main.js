8576427c4a0aa7a914a44c2ce0dd433c71697014
gbombassaro <giovanni.bombassaro@gmail.com>
2022-07-17 11:52:20 -0300

    Atualiza storybook main config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,11 @@
 const path = require('path');
-// const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   "stories": [
-    // "../storybook/**/*.stories.@(ts|tsx|js|mdx)",
-    "../storybook/Button/**/*.stories.@(ts|tsx|js|mdx)"
+    "../storybook/**/*.stories.@(ts|tsx|js|mdx)",
   ],
   "addons": [
     "@storybook/addon-essentials"
   ],
   "framework": "@storybook/react"
-  // webpackFinal: async (config) => {
-  //   config.resolve.plugins = [
-  //     new TsconfigPathsPlugin({
-  //       configFile: path.resolve(__dirname, '../tsconfig.json')
-  //     }),
-  //   ];
-  //   return config;
-  // },
 };
