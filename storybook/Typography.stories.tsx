7e8ad5ef941b72230e2670c2afa85046e97e5fa0
gbombassaro <giovanni.bombassaro@gmail.com>
2022-07-22 00:33:14 -0300

    Configura interfaces da prop as utilizando o componente estilizado e melhora separação de pastas

--- storybook/Typography.stories.tsx
+++ storybook/Typography.stories.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { ComponentMeta } from '@storybook/react'
 
 import { Typography } from '../primitives/Typography/index'
 import { ThemeProvider } from '../providers/Theme'
 import theme from '../tokens/theme.json'
 
-export default { title: 'PrensaDS/Typography', component: Typography } as ComponentMeta<typeof Typography>;
+export default { title: 'PrensaDS/Typography', component: Typography }
 
 export const Variants = () => {
   return (
@@ -51,6 +50,31 @@ export const FontWeight = () => {
           {name} fontWeight
         </Typography>
       ))}
+    </ThemeProvider>
+  )
+}
+
+export const Semantics = () => {
+  return (
+    <ThemeProvider>
+      <Typography as='h1' variant='default-heading-2xl' >
+        The quick brown fox
+      </Typography>
+      <Typography as='h2' variant='default-heading-xl'>
+        The quick brown fox
+      </Typography>
+      <Typography as='h3' variant='default-heading-lg'>
+        The quick brown fox
+      </Typography>
+      <Typography as='h4' variant='default-heading-md'>
+        The quick brown fox
+      </Typography>
+      <Typography as='h5' variant='default-heading-sm'>
+        The quick brown fox
+      </Typography>
+      <Typography as='h6' variant='default-heading-xs'>
+        The quick brown fox
+      </Typography>
     </ThemeProvider>
   )
 }
