fe80b1407fe375c95a2b40cd987065dcb7ffc438
gbombassaro <giovanni.bombassaro@gmail.com>
2022-07-23 12:54:19 -0300

    Atualiza rollup.config

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,17 @@
 import createBundle from './bundle/bundleGenerator'
+import createDeclaration from './bundle/declarationsGenerator'
 
 /**
  * Prensa JS output file
  * All entities must have a index.ts file.
- * This file generates javascript bundles for NPM distribution.
- * Typescript declarations d.ts are NOT generated here, output is JS ONLY.
+ * This file generates javascript bundles for NPM distribution using bundleGenerator.
+ * This file also generates d.ts files using createDeclaration declarationsGenerator.
  */
 export default [
-  createBundle('components/Block/index.ts'),
-  createBundle('components/Typography/index.ts'),
+  createBundle('primitives/Block/index.ts'),
+  createBundle('primitives/Typography/index.ts'),
+  createBundle('providers/PrensaStylesProvider/index.ts'),
+  createDeclaration('primitives/Block/index.ts'),
+  createDeclaration('primitives/Typography/index.ts'),
+  createDeclaration('providers/PrensaStylesProvider/index.ts'),
 ]
