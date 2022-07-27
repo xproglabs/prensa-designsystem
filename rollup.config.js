4af33cc34f603ce89e7a240b6d5fceb081f9041e
gbombassaro <giovanni.bombassaro@gmail.com>
2022-07-27 18:02:08 -0300

    feat: Teste QA v2

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import createDeclaration from './bundle/declarationsGenerator'
  * Prensa JS output file
  * All entities must have a index.ts file.
  * This file generates javascript bundles for NPM distribution using bundleGenerator.
- * This file also generates d.ts files using createDeclaration declarationsGenerator.
+ * This file also generates d.ts files using declarationsGenerator.
  */
 export default [
   createBundle('primitives/Block/index.ts'),
