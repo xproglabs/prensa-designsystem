576f3e3582ff59d98bc88f5fdd8f7f66b2a28464
gbombassaro <giovanni.bombassaro@gmail.com>
2022-08-02 00:03:31 -0300

    Ajustes no PrensaThemeProvider passando para o componente externo a responsabilidade de fornecer o tema

--- providers/PrensaThemeProvider/provider.tsx
+++ providers/PrensaThemeProvider/provider.tsx
@@ -1,30 +1,9 @@
-import { PrensaEngine } from '@PrensaEngine'
 import React from 'react'
 
-export const PrensaThemeProvider = (props) => {
-  const { children, theme, ...otherProps } = props
-  const defaultThemeProps = { colors: { primary: 'yellow' } }
-  const darkThemeProps = { colors: { primary: 'blue' } }
-  const defaultTheme = PrensaEngine.createTheme('default-theme', defaultThemeProps)
-  const darkTheme = PrensaEngine.createTheme('dark-theme', darkThemeProps)
-
-  let selectedTheme
-  switch (theme) {
-    case 'default-theme':
-      selectedTheme = defaultTheme
-      break
-    case 'dark-theme':
-      selectedTheme = darkTheme
-      break
-    default:
-      selectedTheme = defaultTheme
-  }
-  
+export const PrensaThemeProvider = ({ children, theme, ...otherProps }) => {
   return (
-    <div className={selectedTheme}>
-      {React.Children.map(children, (item: any) => (
-        React.cloneElement(item, { ...otherProps })
-      ))}
+    <div className={theme}>
+      {React.Children.map(children, (item: any) => React.cloneElement(item, { theme, ...otherProps }))}
     </div>
   )
 }
