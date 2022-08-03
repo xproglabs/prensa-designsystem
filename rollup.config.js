b2590bda93776d369cf01a61b4f7a9a975d580c8
gbombassaro <giovanni.bombassaro@gmail.com>
2022-08-03 20:37:40 -0300

    Atualiza JSDoc em rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import createBundle from './bundle/bundleGenerator'
  * Prensa JS output file
  * All entities must have a index.ts file.
  * This file generates javascript bundles for NPM distribution using bundleGenerator.
- * This file also generates d.ts files using declarationsGenerator.
  */
 export default [
   createBundle('index.ts'),
