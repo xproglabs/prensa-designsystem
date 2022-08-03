3e212d0d9b4d5123dfa167f7facb704420ab830c
gbombassaro <giovanni.bombassaro@gmail.com>
2022-08-03 20:55:55 -0300

    Atualiza nome dos types de tokens

--- types.ts
+++ types.ts
@@ -2,9 +2,10 @@ import { CSS } from '@stitches/react'
 
 import { PrensaEngine } from './prensa.default.config'
 
-export type CSSProp = CSS<typeof PrensaEngine.config>
-export type ColorTokens = typeof PrensaEngine['config']['theme']['colors']
-export type LineHeightTokens = typeof PrensaEngine['config']['theme']['lineHeights']
-export type RadiiTokens = typeof PrensaEngine['config']['theme']['radii']
-export type SpaceTokens = typeof PrensaEngine['config']['theme']['space']
-export type TypeSystemTokens = typeof PrensaEngine['config']['theme']['fontSizes']
+export type PrensaCSSProp = CSS<typeof PrensaEngine.config>
+export type PrensaThemeType = typeof PrensaEngine['theme']
+export type PrensaColorTokens = typeof PrensaEngine['config']['theme']['colors']
+export type PrensaLineHeightTokens = typeof PrensaEngine['config']['theme']['lineHeights']
+export type PrensaRadiiTokens = typeof PrensaEngine['config']['theme']['radii']
+export type PrensaSpaceTokens = typeof PrensaEngine['config']['theme']['space']
+export type PrensaTypeSystemTokens = typeof PrensaEngine['config']['theme']['fontSizes']
