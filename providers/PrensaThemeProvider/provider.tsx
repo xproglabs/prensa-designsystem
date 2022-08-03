6762261cddedbb337bf455cd7d49e5f0b80d6ae2
gbombassaro <giovanni.bombassaro@gmail.com>
2022-08-03 20:57:39 -0300

    update: Atualiza tipagens para PrensaThemeProvider

--- providers/PrensaThemeProvider/provider.tsx
+++ providers/PrensaThemeProvider/provider.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export const PrensaThemeProvider = ({ children, theme, ...otherProps }) => {
+import { PrensaEngineDefaultConfig } from '../../prensa.default.config'
+import { PrensaThemeProviderProps } from './props'
+
+export const PrensaThemeProvider = (props: PrensaThemeProviderProps) => {
+  const { children, theme, ...otherProps } = props
   return (
     <div className={theme}>
-      {React.Children.map(children, (item: any) => React.cloneElement(item, { theme, ...otherProps }))}
+      {React.Children.map(children, (item: any) => React.cloneElement(item, otherProps))}
     </div>
   )
+}
+
+PrensaThemeProvider.defaultProps = {
+  theme: PrensaEngineDefaultConfig.theme
 }
