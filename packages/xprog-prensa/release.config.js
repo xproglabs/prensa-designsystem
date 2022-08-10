403f9e7c0af79eb204bed8a0ee7bb88a20a44b61
gbombassaro <giovanni.bombassaro@gmail.com>
2022-08-10 20:30:22 -0300

    fix: Configura diretório root para release

--- packages/xprog-prensa/release.config.js
+++ packages/xprog-prensa/release.config.js
@@ -31,7 +31,9 @@ module.exports = {
       '@semantic-release/release-notes-generator',
     ],
     [
-      '@semantic-release/npm'
+      '@semantic-release/npm', {
+        pkgRoot: 'dist'
+      }
     ],
     [
       '@semantic-release/git', {
