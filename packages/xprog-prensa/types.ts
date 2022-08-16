cb44b0c82cba597d43d4c5365665cf9f76202aed
gbombassaro <giovanni.bombassaro@gmail.com>
2022-08-15 21:45:57 -0300

    add: Novo tipo PrensaTypeSystem em @xprog/prensa/types

--- packages/xprog-prensa/types.ts
+++ packages/xprog-prensa/types.ts
@@ -6,7 +6,6 @@ type PrensaColors = typeof PrensaEngine['config']['theme']['colors']
 type PrensaLineHeights = typeof PrensaEngine['config']['theme']['lineHeights']
 type PrensaRadiis = typeof PrensaEngine['config']['theme']['radii']
 type PrensaSpaces = typeof PrensaEngine['config']['theme']['space']
-type PrensaTypeSystem = typeof PrensaEngine['config']['theme']['fontSizes']
 
 export type PrensaCSSProp = CSS<typeof PrensaEngine.config>
 export type PrensaThemeType = typeof PrensaEngine['theme']
@@ -14,6 +13,7 @@ export type PrensaColorTokens = keyof PrensaColors
 export type PrensaLineHeightTokens = keyof PrensaLineHeights
 export type PrensaRadiiTokens = keyof PrensaRadiis
 export type PrensaSpaceTokens = keyof PrensaSpaces
+export type PrensaTypeSystem = typeof PrensaEngine['config']['theme']['fontSizes']
 export type PrensaTypeSystemTokens = keyof PrensaTypeSystem
 
 export * from './primitives/Block/props'
