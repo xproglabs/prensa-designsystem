a4594eb1859f53ec9c777a9653463ae697aeb982
gbombassaro <giovanni.bombassaro@gmail.com>
2022-08-26 12:17:19 -0300

    feat(engine): Update name pattern in types export from xprog-prensa/types.ts

--- packages/xprog-prensa/types.ts
+++ packages/xprog-prensa/types.ts
@@ -1,20 +1,20 @@
 import { CSS } from '@stitches/react'
 
-import { PrensaEngine } from './prensa.default.config'
+import { PrensaEngineType } from './prensa.default.config'
 
-type PrensaColors = typeof PrensaEngine['config']['theme']['colors']
-type PrensaLineHeights = typeof PrensaEngine['config']['theme']['lineHeights']
-type PrensaRadiis = typeof PrensaEngine['config']['theme']['radii']
-type PrensaSpaces = typeof PrensaEngine['config']['theme']['space']
+type PrensaEngineColors = PrensaEngineType['config']['theme']['colors']
+type PrensaEngineLineHeights = PrensaEngineType['config']['theme']['lineHeights']
+type PrensaEngineRadiis = PrensaEngineType['config']['theme']['radii']
+type PrensaEngineSpaces = PrensaEngineType['config']['theme']['space']
+type PrensaEngineFontSizes = PrensaEngineType['config']['theme']['fontSizes']
 
-export type PrensaCSSProp = CSS<typeof PrensaEngine.config>
-export type PrensaThemeType = typeof PrensaEngine['theme']
-export type PrensaColorTokens = keyof PrensaColors
-export type PrensaLineHeightTokens = keyof PrensaLineHeights
-export type PrensaRadiiTokens = keyof PrensaRadiis
-export type PrensaSpaceTokens = keyof PrensaSpaces
-export type PrensaTypeSystem = typeof PrensaEngine['config']['theme']['fontSizes']
-export type PrensaTypeSystemTokens = keyof PrensaTypeSystem
+export type PrensaEngineCSSProp = CSS<PrensaEngineType['config']>
+export type PrensaEngineThemeType = PrensaEngineType['theme']
+export type PrensaEngineColorType = keyof PrensaEngineColors
+export type PrensaEngineLineHeightType = keyof PrensaEngineLineHeights
+export type PrensaEngineRadiiType = keyof PrensaEngineRadiis
+export type PrensaEngineSpaceType = keyof PrensaEngineSpaces
+export type PrensaEngineTypeSystemType = keyof PrensaEngineFontSizes
 
 export * from './primitives/Accordion/props'
 export * from './primitives/Block/props'
