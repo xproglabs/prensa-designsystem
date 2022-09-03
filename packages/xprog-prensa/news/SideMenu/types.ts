b22a7f039c60dfd27390894188b7192ea88db165
gbombassaro <giovanni.bombassaro@gmail.com>
2022-09-03 17:52:18 -0300

    add: Export SideMenu types and SideMenuContentType, SideMenuFooterType, SideMenuHeaderType

--- packages/xprog-prensa/news/SideMenu/types.ts
+++ packages/xprog-prensa/news/SideMenu/types.ts
@@ -1,6 +1,11 @@
 import { ReactNode, MouseEventHandler } from 'react'
 
 import { DrawerProps, PrensaEngineCSSProp } from '../../types'
+import { SideMenuContentType, SideMenuFooterType, SideMenuHeaderType } from './styles'
+
+export type SideMenuContentProps = SideMenuContentType
+export type SideMenuFooterProps = SideMenuFooterType
+export type SideMenuHeaderProps = SideMenuHeaderType
 
 export type SideMenuProps = {
   close: MouseEventHandler<HTMLDivElement>;
