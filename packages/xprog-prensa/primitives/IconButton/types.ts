290bd0f274d8c4329874b2cc65dba7f6ca569ba9
gbombassaro <giovanni.bombassaro@gmail.com>
2022-09-10 21:11:42 -0300

    fix: IconButton variant type correspondency and change filename props to types

--- packages/xprog-prensa/primitives/IconButton/types.ts
+++ packages/xprog-prensa/primitives/IconButton/types.ts
@@ -1,11 +1,15 @@
+import { VariantProps } from '@stitches/react'
 import { ReactNode } from 'react'
+
 import { PrensaEngineColorType } from '../../types'
 import { StyledIconButtonTypes } from './styles'
+
+type IconButtonVariants = VariantProps<StyledIconButtonTypes>
 
 export type IconButtonProps = StyledIconButtonTypes['defaultProps'] & {
   color?: PrensaEngineColorType;
   icon?: ReactNode;
   iconColor?: PrensaEngineColorType;
-  variant?: 'filled' | 'outlined' | 'ghost';
+  variant?: IconButtonVariants['variant'];
   viewBox?: string;
 }
