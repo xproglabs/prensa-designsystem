ff0331f27eaf4d116f8de79f84d6c77da582dc68
gbombassaro <giovanni.bombassaro@gmail.com>
2023-01-05 20:21:21 -0300

    Improvement in component syntax

--- packages/xprog-prensa/primitives/ImageGallery/index.tsx
+++ packages/xprog-prensa/primitives/ImageGallery/index.tsx
@@ -8,16 +8,37 @@ import { WebImageGalleryProps } from './types'
 
 const WebImageGallery = ({
   captionProps,
-  componentCaption: RenderCaption,
-  componentImage: Image,
+  componentCaption,
+  componentImage,
   items
 }: WebImageGalleryProps) => {
+
+  const RenderCaption = componentCaption
+  const RenderImage = componentImage
+
   const renderArrowPrev = (onClick, hasArrow) => {
     return hasArrow && <ControlButton onClick={onClick} />
   }
 
   const renderArrowNext = (onClick, hasArrow) => {
     return hasArrow && <ControlButton onClick={onClick} isNext />
+  }
+
+  const renderCaptionOriginalOrCustom = (item) => {
+    if (captionProps.enabled && RenderCaption) {
+      return (
+        <RenderCaption
+          captionProps={captionProps}
+          textValue={item.caption}
+        />
+      )
+    } else {
+      return (
+        <Caption {...captionProps}>
+          {item.caption}
+        </Caption>
+      )
+    }
   }
 
   return (
@@ -30,13 +51,8 @@ const WebImageGallery = ({
     >
       {map(items, (item, key) => (
         <React.Fragment key={key}>
-          <Image alt={item?.caption} src={item?.url} />
-          {captionProps?.enabled && RenderCaption && (
-            <RenderCaption captionProps={captionProps} textValue={item.caption} />
-          )}
-          {captionProps?.enabled && !RenderCaption && (
-            <Caption {...captionProps}>{item.caption}</Caption>
-          )}
+          <RenderImage alt={item?.caption} src={item?.url} />
+          {renderCaptionOriginalOrCustom(item)}
         </React.Fragment>
       ))}
     </Carousel>
