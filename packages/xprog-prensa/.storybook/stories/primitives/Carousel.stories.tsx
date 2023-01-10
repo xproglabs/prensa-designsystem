b55ed469ccb56c1bbce81055cca768d1363a2d12
gbombassaro <giovanni.bombassaro@gmail.com>
2023-01-10 13:28:15 -0300

    Update examples for Carousel

--- packages/xprog-prensa/.storybook/stories/primitives/Carousel.stories.tsx
+++ packages/xprog-prensa/.storybook/stories/primitives/Carousel.stories.tsx
@@ -1,38 +1,22 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
 import React from 'react'
-import { ArgsTable, Title } from '@storybook/addon-docs'
 
+import { Block } from '../../../primitives/Block'
 import { Carousel } from '../../../primitives/Carousel'
 import { StorybookTheme } from '../../StorybookTheme'
 
-export default {
-  title: 'Primitives/Carousel',
-  component: Carousel,
-  parameters: {
-    docs: {
-      page: () => (
-        <>
-          <Title />
-          <ArgsTable
-            components={{
-              Carousel
-            }}
-          />
-        </>
-      )
-    }
-  }
-}
+export default { title: 'Primitives/Carousel', component: Carousel }
 
-const Template = (args) => {
+const Template: ComponentStory<any> = (args) => {
   return (
     <div className={StorybookTheme.className}>
       <Carousel
-        animationHandler='slide'
-        centerSlidePercentage={0}
         axis='horizontal'
+        labels={{ item: 'Item', leftArrow: 'Retroceder', rightArrow: 'Avançar' }}
         showStatus={false}
-        thumbWidth={80}
-        width='800px'
+        showThumbs={false}
+        swipeable={true}
+        {...args}
       >
         <div key='slide1'>
           <img src='https://images.unsplash.com/photo-1516306580123-e6e52b1b7b5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3926&q=80' />
@@ -43,9 +27,48 @@ const Template = (args) => {
         <div key='slide3'>
           <img src='https://images.unsplash.com/photo-1554168848-228452c09d60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3132&q=80' />
         </div>
+        <div key='slide4'>
+          <img src='https://images.unsplash.com/photo-1623511792208-9339352198cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80' />
+        </div>
+      </Carousel>
+    </div>
+  )
+}
+const TemplateWithDivs: ComponentStory<any> = (args) => {
+  return (
+    <div className={StorybookTheme.className}>
+      <Carousel
+        axis='horizontal'
+        labels={{ item: 'Item', leftArrow: 'Retroceder', rightArrow: 'Avançar' }}
+        showStatus={false}
+        showThumbs={false}
+        swipeable={true}
+        {...args}
+      >
+        <Block key='1' css={{ backgroundColor: '$basicError500', height: '100px', width: '100px' }} />
+        <Block key='2' css={{ backgroundColor: '$basicError400', height: '100px', width: '100px' }} />
+        <Block key='3' css={{ backgroundColor: '$basicError300', height: '100px', width: '100px' }} />
+        <Block key='4' css={{ backgroundColor: '$basicError200', height: '100px', width: '100px' }} />
       </Carousel>
     </div>
   )
 }
 
 export const Default = Template.bind({})
+Default.args = {}
+export const WithThumbs = Template.bind({})
+WithThumbs.args = { showThumbs: true }
+export const Customized = TemplateWithDivs.bind({})
+Customized.args = {
+  height: '800px',
+  width: '100%',
+  showArrows: false,
+  css: {
+    controlDotsDot: {
+      backgroundColor: '$basicError500',
+      opacity: 0.8,
+      height: '12px',
+      width: '12px'
+    }
+  }
+}
