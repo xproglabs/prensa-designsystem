bca3a054d9d5fc045c0ac6e6fb2d0edfe36f19df
gbombassaro <giovanni.bombassaro@gmail.com>
2023-05-23 20:35:00 -0300

    fix: Update types for Typography as and variant properties

--- packages/xprog-prensa/primitives/Typography/types.ts
+++ packages/xprog-prensa/primitives/Typography/types.ts
@@ -1,8 +1,8 @@
-import { PrensaEngineCSS, PrensaEngineTypeof } from '@xprog/prensa-system'
+import { PrensaEngineTypeof } from '@xprog/prensa-system'
 import { ITypographyStyled } from './styled'
 
-export type TypographyVariantKeys = { [K in keyof PrensaEngineTypeof['config']['theme']['fontSizes']]: PrensaEngineCSS };
-export type TypographyElementKeys = Pick<JSX.IntrinsicElements, 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span' | 'cite' | 'li'>;
+export type TypographyVariantKeys = keyof PrensaEngineTypeof['config']['theme']['fontSizes'];
+export type TypographyElementKeys = keyof Pick<JSX.IntrinsicElements, 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span' | 'cite' | 'li'>;
 
 export interface ITypography extends ITypographyStyled {
   as?: TypographyElementKeys;
