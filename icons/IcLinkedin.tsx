62d78d15a4ccb7c38c34204beda52179a545b268
mateus <mateusrpinheiro.contato@outlook.com>
2023-07-25 12:38:14 -0300

    change iclinkedin color

--- icons/IcLinkedin.tsx
+++ icons/IcLinkedin.tsx
@@ -4,7 +4,9 @@ import React from 'react'
 const IconLinkedin = (props) => {
   return (
     <Icon xmlns='http://www.w3.org/2000/svg' {...props}>
-      <path d='M3.709 9.087v11.346h3.538V9.087H3.709Zm-.154-3.57a1.935 1.935 0 1 0 1.935-1.95 1.942 1.942 0 0 0-1.935 1.95Zm13.391 14.916h3.499v-6.93c0-6.105-6.333-5.59-7.57-2.757V9.087h-3.48v11.346h3.635V14.12c0-2.86 3.916-2.834 3.916 0v6.312Z'/>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="#0a66c2" className="mercado-match" width="24" height="24" focusable="false">
+        <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+      </svg>
     </Icon>
   )
 }
