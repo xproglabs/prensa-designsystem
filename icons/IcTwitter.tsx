10e50adac76b07e1c1af5d12335f1da4d8a9d71b
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2024-01-16 18:53:07 -0300

    Remove none properties from IcTwitter

--- icons/IcTwitter.tsx
+++ icons/IcTwitter.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const IconTwitter = (props) => {
   return (
     <Icon xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" {...props}>
-      <g fill="none" fillRule="none" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none">
+      <g strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDashoffset="0">
         <g transform="scale(5.33333,5.33333)">
           <path d="M41,6l-31.071,36h-3.714l31.072,-36z" fill="#a8a8a8" fillRule="nonzero"></path>
           <path d="M31.143,41l-23.323,-34h8.957l23.323,34z" fill="transparent" fillRule="evenodd"></path>
